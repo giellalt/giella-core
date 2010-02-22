ca649a3db666b548b857f55e24a608d61ba72377
Trond Trosterud <trond.trosterud@uit.no>
2010-02-22 07:40:25 +0000

    forgotten a context (the empty node context), and a tag, ex.

--- dicts/scripts/tagharmonising.pl
+++ dicts/scripts/tagharmonising.pl
@@ -4,33 +4,46 @@ use utf8 ;
 while (<>)
 
 {
-s/trgr>/tg>/g ;
-s/exgr>/xg>/g ;
-s/syngr>/syng>/g ;
-s/refgr>/refg>/g ;
-s/extr>/xt>/g ;
-s/rootdict>/r>/g ;
-s/entry>/e>/g ;
-s/mgr>/mg>/g ;
-s/trans>/t>/g ;
-s/<trans/<t/g ;
 s/<lemma/<l/g ;
+s/<trans/<t/g ;
+s/entry>/e>/g ;
+s/entry\/>/e\/>/g ;
+s/ex>/x>/g ;
+s/ex\/>/x\/>/g ;
+s/exgr>/xg>/g ;
+s/exgr\/>/xg\/>/g ;
+s/extr>/xt>/g ;
+s/extr\/>/xt\/>/g ;
 s/lemma>/l>/g ;
+s/lemma\/>/l\/>/g ;
+s/mgr>/mg>/g ;
+s/mgr\/>/mg\/>/g ;
+s/refgr>/refg>/g ;
+s/refgr\/>/refg\/>/g ;
 s/restr>/re>/g ;
+s/restr\/>/re\/>/g ;
+s/rootdict>/r>/g ;
+s/syngr>/syng>/g ;
+s/syngr\/>/syng\/>/g ;
+s/trans>/t>/g ;
+s/trans\/>/t\/>/g ;
+s/trgr>/tg>/g ;
+s/trgr\/>/tg\/>/g ;
 
-s/\"trgr\"/\"tg\"/g ;
-s/\"exgr\"/\"xg\"/g ;
-s/\"syngr\"/\"syng\"/g ;
-s/\"refgr\"/\"refg\"/g ;
-s/\"extr\"/\"xt\"/g ;
-s/\"rootdict\"/\"r\"/g ;
-s/\"entry\"/\"e\"/g ;
-s/\"mgr\"/\"mg\"/g ;
-s/\"trans\"/\"t\"/g ;
-s/\"<trans/<t/g ;
 s/\"<lemma/\"<l/g ;
+s/\"<trans/<t/g ;
+s/\"entry\"/\"e\"/g ;
+s/\"ex\"/\"x\"/g ;
+s/\"exgr\"/\"xg\"/g ;
+s/\"extr\"/\"xt\"/g ;
 s/\"lemma\"/\"l\"/g ;
+s/\"mgr\"/\"mg\"/g ;
+s/\"refgr\"/\"refg\"/g ;
 s/\"restr\"/\"re\"/g ;
+s/\"rootdict\"/\"r\"/g ;
+s/\"syngr\"/\"syng\"/g ;
+s/\"trans\"/\"t\"/g ;
+s/\"trgr\"/\"tg\"/g ;
 
 print ;
 }

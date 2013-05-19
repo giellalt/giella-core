f367a0093a11b67d759da0fe01ddc013e15b0429
sjur <sjur@c7155fb1-f0a7-4240-a2fc-2600b6f42f90>
2013-05-19 20:31:10 +0000

    Fixed a few errors: / should be replaced with _, and there should be no escaping in the relabel file. The filename of the output file is now given in the second option.
    git-svn-id: https://gtsvn.uit.no/langtech/trunk/gtcore@76433 c7155fb1-f0a7-4240-a2fc-2600b6f42f90

--- scripts/gttags2apertium.sh
+++ scripts/gttags2apertium.sh
@@ -5,17 +5,20 @@
 # tags. This regex can then be used to produce Apertium fst's from standard
 # fst's.
 #
-# Usage: $0 GTLANG.lexc.fst
+# Usage: $0 GTLANG.lexc.fst OUTFILE
 #
 # GTLANG.lexc.fst is the input lexical transducer, which should contain all tags
 # used; this guarantees that all tags are transformed to Apertium style.
+# OUTFILE is the source file to be generated.
 
 hfst-project -p upper $1 | hfst-minimise | hfst-summarise -v | grep -A1 'arc symbols actually seen' | grep -F '+' | tr ' ' '\n' | grep '\+[^,]' | tr -d ',' > $1.gttags
 
-tr -d '+' < $1.gttags | tr '[:upper:]' '[:lower:]' | awk '{print "<"$0">"}' > $1.apertiumtags
+tr -d '+' < $1.gttags | tr '/[:upper:]' '_[:lower:]' | awk '{print "%<"$0"%>"}' > $1.apertiumtags
 
 # Either this:
-paste $1.gttags $1.apertiumtags | awk '{print $0","}' | sed 's/	/ -> /g' | sed '$ s/,/;/' > apertium.regex
+paste $1.gttags $1.apertiumtags | awk '{print $0","}' | sed 's/	/ -> /g' | sed '$ s/,/;/' | sed 's/^/%/' > $2
 
 # or this:
-paste $1.gttags $1.apertiumtags > apertium.relabel
+paste $1.gttags $1.apertiumtags | tr -d '%' > $2.relabel
+
+rm -r $1.gttags $1.apertiumtags

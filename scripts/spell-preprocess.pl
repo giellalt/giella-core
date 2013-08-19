750a3e294b5627e065935af811ba4fafb42cdf96
sjur <sjur@c7155fb1-f0a7-4240-a2fc-2600b6f42f90>
2013-08-19 22:44:18 +0000

    Svn cp from old infra.
    git-svn-id: https://gtsvn.uit.no/langtech/trunk/gtcore@79477 c7155fb1-f0a7-4240-a2fc-2600b6f42f90

---
+++ scripts/spell-preprocess.pl
@@ -0,0 +1,41 @@
+#!/usr/bin/perl -w
+#
+# spell-preprocess.pl
+# Preprocesses input data before spell checking, to ensure
+# consistent and valid data. The output format follows typos.txt. 
+#
+# Usage: spell-preprocess.pl < INFILE > OUTFILE
+
+use strict;
+use utf8;
+
+# These definitions ensure that the script works
+# also in environments, where PERL_UNICODE is not set.
+binmode( STDIN, ':utf8' );
+binmode( STDOUT, ':utf8' );
+binmode( STDERR, ':utf8' );
+use open 'utf8';
+
+while(<>) {
+	# if the line contains a tab, there is an error correction pair - all is ok:
+	if (/.+\t.+/) { print; next; }
+
+	# discard lines which don't contain any letters.
+	next if (! /^\p{L}/);
+
+	chomp;
+
+	# replace anything that is not a letter or dash
+	# from the beginning and end of the expression.
+	# Preserves full stop at the end.
+	s/^[^\p{L}\p{Pd}]+//;
+	s/[^\p{L}\p{Pd}\.]+$//;
+
+	# remove the full stop if preceded by punctuation 
+	# for example: word).
+	s/[^\p{L}]+\.$//;
+
+	# add a tab and a newline to the end of the word, then print:
+	my $word = $_ . "\t\n";
+	print $word;
+}

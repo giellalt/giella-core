cbe910f458e68bac581f7fdd0c53dfaf30d6b1a6
sjur <sjur@c7155fb1-f0a7-4240-a2fc-2600b6f42f90>
2013-11-24 19:16:49 +0000

    Shell script to take a list of tags and turn it into a regex to make those tags optional.
    git-svn-id: https://gtsvn.uit.no/langtech/trunk/gtcore@84044 c7155fb1-f0a7-4240-a2fc-2600b6f42f90

---
+++ scripts/taglist2make_optional_tags_regex.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Take a list of GT/Divvun tags of the form +Abc/XYZ, and turn it into a regex
+# to remove those tags.
+
+SED=sed
+
+$SED   's/+/%+/g' $1 \
+| $SED 's/\//%\//g' \
+| $SED 's/^/0 (<-) /' \
+| $SED 's/$/,/' \
+| $SED '$ s/,/;/'

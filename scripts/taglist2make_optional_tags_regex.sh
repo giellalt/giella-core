9b4a8ea9c09fa6caf15cdaf2d3cf9b8d881813f9
sjur <sjur@c7155fb1-f0a7-4240-a2fc-2600b6f42f90>
2013-11-25 08:04:56 +0000

    Corrected a comment, added comments to explain the scripts, and simplified the steps to escape characters reserved in xfst.
    git-svn-id: https://gtsvn.uit.no/langtech/trunk/gtcore@84049 c7155fb1-f0a7-4240-a2fc-2600b6f42f90

--- scripts/taglist2make_optional_tags_regex.sh
+++ scripts/taglist2make_optional_tags_regex.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
 # Take a list of GT/Divvun tags of the form +Abc/XYZ, and turn it into a regex
-# to remove those tags.
+# to make those tags optional.
 
 SED=sed
 
-$SED   's/+/%+/g' $1 \
-| $SED 's/\//%\//g' \
+# Script explanation:
+# 1. the first line contains tag symbols that need to be escaped - add as needed
+# 2. the second line adds the xfst operation at the beginning of each line
+# 3. the third line adds a comma at the end of each line
+# 4. the fourth line replaces the last comma with a semicolon, to end the regex
+
+$SED   's/\([+/-]\)/%\1/g' $1 \
 | $SED 's/^/0 (<-) /' \
 | $SED 's/$/,/' \
 | $SED '$ s/,/;/'
--- scripts/taglist2remove_tags_regex.sh
+++ scripts/taglist2remove_tags_regex.sh
@@ -5,8 +5,13 @@
 
 SED=sed
 
-$SED   's/+/%+/g' $1 \
-| $SED 's/\//%\//g' \
+# Script explanation:
+# 1. the first line contains tag symbols that need to be escaped - add as needed
+# 2. the second line adds the xfst operation at the beginning of each line
+# 3. the third line adds a comma at the end of each line
+# 4. the fourth line replaces the last comma with a semicolon, to end the regex
+
+$SED   's/\([+/-]\)/%\1/g' $1 \
 | $SED 's/^/0 <- /' \
 | $SED 's/$/,/' \
 | $SED '$ s/,/;/'

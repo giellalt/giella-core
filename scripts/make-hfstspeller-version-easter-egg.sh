353aeac1d6a1b55ba4711b3c69867b0fac50bc46
sjur <sjur@c7155fb1-f0a7-4240-a2fc-2600b6f42f90>
2014-01-10 16:18:35 +0000

    A simple shell script to create version info easter egg data for hfst-based spellers.
    git-svn-id: https://gtsvn.uit.no/langtech/trunk/gtcore@86437 c7155fb1-f0a7-4240-a2fc-2600b6f42f90

---
+++ scripts/make-hfstspeller-version-easter-egg.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+if test -z "${GTCORE}" ; then
+    echo "Unable to determine GTCORE, re-run gtsetup.sh and re-try"
+    exit 1
+fi
+
+# Wrong usage - short instruction:
+if ! test $# -eq 3 ; then
+    echo
+    echo "Usage: $0 LANGUAGE_CODE ROOT_LANG_DIR VERSION_FILE"
+    echo
+    echo "where:"
+    echo "   LANGUAGE_CODE=iso639 code of the wanted language"
+    echo "   ROOT_LANG_DIR=\$top_srcdir (the dir with the configure.ac file)"
+    echo "   VERSION_FILE=file containing the speller version info"
+    echo
+    exit 1
+fi
+
+Language=$(${GTCORE}/scripts/iso639-to-name.sh $1)
+VersionFile=$3
+
+Date=$(date +%Y.%m.%d)
+Version=$(cat $VersionFile)
+Revision=$(svn info $2 | grep Revision | grep -Eo '[0-9]+')
+HfstVersion=$(hfst-info | grep 'HFST version' | grep -Eo '[0-9.]+')
+HfstRevision=$(hfst-info | grep 'revision' | grep -Eo '[0-9.]+')
+
+echo "Divvun for $Language ($1)"
+echo "Version $Version, $Date, r$Revision"
+echo "Built using HFST $HfstVersion, rev$HfstRevision"

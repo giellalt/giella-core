d4094c3acd98d7f8ab471d4dece015d7e647cbf6
sjur <sjur@c7155fb1-f0a7-4240-a2fc-2600b6f42f90>
2014-03-25 16:23:45 +0000

    Added shell script to generate dialect filtering regexes from a given list of dialect tags and the requested dialect tag. This script is required for the new dialect Oahpa fst support that is about to be committed. Increased GTD core version number.
    git-svn-id: https://gtsvn.uit.no/langtech/trunk/gtcore@91099 c7155fb1-f0a7-4240-a2fc-2600b6f42f90

---
+++ scripts/generate-dialect-filter-regex.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+# Create a filter regex to remove all strings not belonging to the requested
+# dialect, either because they are positively marked for other dialects, or
+# because they are negatively marked for the dialect.
+
+if ! test $# -eq 3 ; then
+    echo
+    echo "Usage: $0 OUTFILE DIALECTTAG ALLDIALECTTAGS"
+    echo
+    echo "OUTFILE        = regex file to be created"
+    echo "DIALECTTAG     = the tag for the target dialect"
+    echo "ALLDIALECTTAGS = a list of tags for all dialeccts"
+    echo
+    exit 1
+fi
+
+REGEXFILE=$1
+TARGETDIAL=$2
+ALLDIALECTS=$3
+SED=sed
+
+NONTARGETDIALECTS=$(echo "$ALLDIALECTS" | tr ' ' '\n' | grep -v $TARGETDIAL )
+
+# echo "Nontargets: $NONTARGETDIALECTS"
+
+# Print header text:
+echo "# This is a generated file - do not edit!"        > $REGEXFILE
+echo "# It removes dialect-marked strings belonging"   >> $REGEXFILE
+echo "# to all dialects but $TARGETDIAL, so that the"  >> $REGEXFILE
+echo "# filtered fst will generate wordforms suitable" >> $REGEXFILE
+echo "# for the target dialect only."                  >> $REGEXFILE
+echo ""                                                >> $REGEXFILE
+
+# Print the actual regular expression:
+echo "~\$[ %+Dial%/%-$TARGETDIAL |" >> $REGEXFILE # Exclude from target
+
+echo "$NONTARGETDIALECTS"  \
+| $SED 's/^/	\+Dial\//' \
+| $SED 's/\([+/-]\)/%\1/g' \
+| $SED 's/$/  |/'          \
+| $SED '$ s/|/ ] ;/' >> $REGEXFILE

1b4d03f67e94f39088bc8ee2394bfa8fe1acf4fa
sjur <sjur@c7155fb1-f0a7-4240-a2fc-2600b6f42f90>
2014-04-11 09:12:35 +0000

    Generalised the extract-semantic-tags.sh script, to extract any set of tags matching the pattern given as the last option to the shell script. Increased the gtd core version number, since this change will break backwards compatibility.
    git-svn-id: https://gtsvn.uit.no/langtech/trunk/gtcore@92168 c7155fb1-f0a7-4240-a2fc-2600b6f42f90

---
+++ scripts/extract-tags.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# From a list of GT/Divvun tags of the form +Abc/XYZ, extract all semantic tags
+# of the form +Sem/abc; if no such tags are found, print a dummy tag instead, to
+# make the generated regex valid.
+
+if ! test $# -eq 3 ; then
+    echo
+    echo "Usage: $0 INPUTFILE OUTPUTFILE MATCHSTRING"
+    echo
+    echo "INPUTFILE   = file with list of all tags in a lexical transducer"
+    echo "OUTPUTFILE  = the subset of tags matching the regex ${REGEX}"
+    echo "MATCHSTRING = the string used to match the set of target tags"
+    echo
+    exit 1
+fi
+
+REGEX=$3
+
+if ! grep -F "${REGEX}" $1; then
+    echo "${REGEX}DummyTag" > $2
+else
+    grep -F "${REGEX}" $1 | cat - > $2
+fi

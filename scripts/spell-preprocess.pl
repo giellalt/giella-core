e47064e44b69ab39d3256dcca5509e5c01691178
sjur <sjur@c7155fb1-f0a7-4240-a2fc-2600b6f42f90>
2014-11-04 07:02:01 +0000

    Allow only digits 0-9 (in addition to letters) in input strings, that is, strings not containing any of those will be skipped. Also remove a few more punctuation chars from the beginning and end of the input strings.
    git-svn-id: https://gtsvn.uit.no/langtech/trunk/gtcore@102194 c7155fb1-f0a7-4240-a2fc-2600b6f42f90

--- scripts/spell-preprocess.pl
+++ scripts/spell-preprocess.pl
@@ -21,7 +21,7 @@ while(<>) {
 	if (/.+\t.+/) { print; next; }
 
 	# discard lines which don't contain any letters or numbers.
-	next if (! /[\p{L}\p{N}]+/);  # No letters or numbers
+	next if (! /[\p{L}\p{Nd}]+/); # No letters or numbers
 	next if (/^\p{P}+$/);         # Remove lines with only punctuation
 	next if (/^[\p{P}\p{S}]+[\p{L}\p{N}]$/); # Remove smilies
 
@@ -29,8 +29,8 @@ while(<>) {
 
 	# Remove a few punctuation chars from the beginning and end of
 	# the expression, preserving full stops at the end.
-	s/^[".\(\/]+//;  # Remove from the start of the word
-	s/["),!?]+\.?$//; # Remove from the end, including a final full stop
+	s/^[".\(\/«”“]+//;  # Remove from the start of the word
+	s/["),!?»”“]+\.?$//; # Remove from the end, including a final full stop
 	s/[.]+$/./; # Collapse multiple full stops to one
 
 	# add a tab and a newline to the end of the word, then print:

9a71acdde9fa1446de8878bf34253078446b1314
sjur <sjur@c7155fb1-f0a7-4240-a2fc-2600b6f42f90>
2015-02-10 14:27:02 +0000

    Renamed and generalised the shell script to create a regex to remove strings with the specified tags in. This is used to automatically create a regex to remove certain categories of tagged strings. The relevant tags have already been extracted to a file used as input (by the make file). Updated the gtcore version at the same time - the new features require the new version of the core.
    git-svn-id: https://gtsvn.uit.no/langtech/trunk/gtcore@107114 c7155fb1-f0a7-4240-a2fc-2600b6f42f90

--- scripts/taglist2remove_strings_regex.sh
+++ scripts/taglist2remove_strings_regex.sh
@@ -1,23 +1,29 @@
 #!/bin/bash
 
 # Create a filter regex to remove all strings not belonging to the requested
-# dialect, either because they are positively marked for other dialects, or
-# because they are negatively marked for the dialect.
+# category, as extracted from the list of multichar symbols in the lexical fst.
 
 if ! test $# -eq 2 -o $# -eq 3 ; then
     echo
-    echo "Usage: $0 OUTFILE TAGFILE [ADAPTIONREGEXFILE]"
+    echo "Usage: $0 OUTFILE CATEGORY TAGFILE [ADAPTIONREGEXFILE]"
     echo
     echo "OUTFILE           = regex file to be created"
-    echo "TAGFILE           = a file with the list of derivation tags"
+    echo "CATEGORY          = a string that describes what is being removed"
+    echo "TAGFILE           = a file with the list of tags for CATEGORY"
     echo "ADAPTIONREGEXFILE = a regex fragment used to modify the final regex"
+    echo "                    - strings with tags listed in this file within []"
+    echo "                      will NOT be removed. Example:"
+    echo "                      [ +Sem/Hum | +Sem/Plc ]"
+    echo "                      Strings with these tags would not be removed"
+    echo "                      from the fst if specified in ADAPTIONREGEXFILE."
     echo
     exit 1
 fi
 
 REGEXFILE=$1
-TAGFILE=$2
-ADAPTIONREGEXFILE=$3
+CATEGORY=$2
+TAGFILE=$3
+ADAPTIONREGEXFILE=$4
 SED=sed
 
 ADAPTIONREGEX=""
@@ -28,9 +34,9 @@ fi
 
 # Print header text:
 echo "# This is a generated file - do not edit!"             > $REGEXFILE
-echo "# The generated regex removes all derivation strings" >> $REGEXFILE
+echo "# The generated regex removes all $CATEGORY strings"  >> $REGEXFILE
 echo "# except whatever is specified in the file given as"  >> $REGEXFILE
-echo "# the third argument (optional). It will remove"      >> $REGEXFILE
+echo "# the last argument (optional). It will remove"       >> $REGEXFILE
 echo "# all derivation strings with no third argument."     >> $REGEXFILE
 echo ""                                                     >> $REGEXFILE
 

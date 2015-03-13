9d01e01db81f7914fca970604de1e49cde89640e
sjur <sjur@c7155fb1-f0a7-4240-a2fc-2600b6f42f90>
2015-03-13 09:25:09 +0000

    Added documenting comments. Made the output of non-generated word forms equal to what is given by the lookup tool.
    git-svn-id: https://gtsvn.uit.no/langtech/trunk/gtcore@109219 c7155fb1-f0a7-4240-a2fc-2600b6f42f90

--- scripts/word_form_cohorts-to-table.pl
+++ scripts/word_form_cohorts-to-table.pl
@@ -1,5 +1,21 @@
 #!/usr/bin/env perl -w
 
+# This Perl script takes three input argument, and produces an html table in
+# the file specified in the fourth argument. The arguments are:
+#
+# --input       - generated word forms in xerox style cohorts
+# --lemlex      - the lemmas and their corresponding continuation lexicons,
+#                 as given as input to the word form generation
+# --codelist    - list of morphosyntactic tags used to generate word forms
+# --output      - name of file containing the produced html table
+#
+# The cohorts are read and put into a table with the corresponding lemma and
+# continuation lexicon. The list of morphosyntactic tags is used as column
+# headers.
+#
+# This script is part of a number of scripts that together produce an overview
+# of what the specified fst will produce in terms of word forms.
+
 use strict;
 use utf8;
 use open 'utf8';
@@ -17,9 +33,9 @@ my $number_of_codes;
 my @code_array;
 
 GetOptions ("input=s" => \$word_form_file,
-            "output=s" => \$word_form_table_file,
             "lemlex=s" => \$lemma_lexicon_list,
-            "codelist=s" => \$code_list );
+            "codelist=s" => \$code_list,
+            "output=s" => \$word_form_table_file );
 
 $code_list =~ s/ +/ /g;
 $number_of_codes = @code_array = split(' ',$code_list);
@@ -76,7 +92,7 @@ for (my $ix = 0; $ix <= $#word_form_array; $ix += $number_of_codes ) {
         for my $wordform (@word_forms) {
             my ($input, $word, $questionmark) = split ('\t',$wordform);
             if ($questionmark) {
-                print HTMLTABLE "???</br>";
+                print HTMLTABLE "$questionmark";
             }
             else {
                 print HTMLTABLE "$word</br>";

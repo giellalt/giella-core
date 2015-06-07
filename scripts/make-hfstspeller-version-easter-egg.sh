ebde6e661a94a52c42e2c35c41d22031245f6f67
sjur <sjur@c7155fb1-f0a7-4240-a2fc-2600b6f42f90>
2015-06-07 19:18:49 +0000

    Support for optional variant information in the version easter egg.
    git-svn-id: https://gtsvn.uit.no/langtech/trunk/gtcore@115068 c7155fb1-f0a7-4240-a2fc-2600b6f42f90

--- scripts/make-hfstspeller-version-easter-egg.sh
+++ scripts/make-hfstspeller-version-easter-egg.sh
@@ -13,27 +13,37 @@ else
 fi
 
 # Wrong usage - short instruction:
-if ! test $# -eq 3 ; then
+if ! test $# -eq 3 -o $# -eq 4 ; then
     echo
-    echo "Usage: $0 LANGUAGE_CODE ROOT_LANG_DIR VERSION_FILE"
+    echo "Usage: $0 LANGUAGE_CODE ROOT_LANG_DIR VERSION_FILE [VARIANT]"
     echo
     echo "where:"
     echo "   LANGUAGE_CODE=iso639 code of the wanted language"
     echo "   ROOT_LANG_DIR=\$top_srcdir (the dir with the configure.ac file)"
     echo "   VERSION_FILE=file containing the speller version info"
+    echo "   VARIANT=variant tag (script code, country code, alt. spelling)"
     echo
     exit 1
 fi
 
 Language=$(${MYGTCORE}/scripts/iso639-to-name.sh $1)
-
-Date=$(date +%d.%m.%Y)
-Version=$(cat $3)
 Revision=$(svn info --xml $2 | grep -A 4 '<entry' \
 		 | grep revision | grep -Eo '[0-9]+')
+Version=$(cat $3)
+Variant=$4
+
+Date=$(date +%d.%m.%Y)
 HfstVersion=$(hfst-info | grep 'HFST version' | grep -Eo '[0-9.]+')
 HfstRevision=$(hfst-info | grep 'revision' | grep -Eo '[0-9.]+')
 
-echo "Divvun speller for $Language"
-echo "$1 version $Version, $Date, rev$Revision"
+if test $Variant == 'default' -o "x$Variant" == 'x'; then
+    VariantText=""
+    Variant=""
+else
+    VariantText=" using $Variant"
+    Variant="-$Variant"
+fi
+
+echo "Divvun speller for $Language$VariantText"
+echo "$1$Variant version $Version, $Date, rev$Revision"
 echo "Built using HFST $HfstVersion, rev$HfstRevision"

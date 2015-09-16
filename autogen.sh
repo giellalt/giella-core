abf39db42b70cdc8adde548e9b776534a0175ec6
unhammer <unhammer@c7155fb1-f0a7-4240-a2fc-2600b6f42f90>
2015-09-16 10:44:47 +0000

    install gtdshared and scripts from dist
    git-svn-id: https://gtsvn.uit.no/langtech/trunk/gtcore@121084 c7155fb1-f0a7-4240-a2fc-2600b6f42f90

--- autogen.sh
+++ autogen.sh
@@ -2,5 +2,10 @@
 
 echo "Initial automake setup of $(basename $(pwd))"
 
+echo 'subdir_files = \' > subfiles.mk
+find gtdshared scripts -type f -print | \
+    grep -vE '/(Makefile|.*\.(am|in|ac)$|configure|config\.|autogen\.sh|build-aux|autom4te.cache|aclocal\.m4)' |\
+    sed 's/^/   /;$q;s/$/ \\/' >> subfiles.mk
+
 # autoreconf should work for most platforms
 autoreconf -i -v

b6bf346ffa2c79274b2b3c646b7e2e5286b60212
boerre <boerre@c7155fb1-f0a7-4240-a2fc-2600b6f42f90>
2016-10-27 19:35:17 +0000

    Style improvements
    git-svn-id: https://gtsvn.uit.no/langtech/trunk/giella-core@142278 c7155fb1-f0a7-4240-a2fc-2600b6f42f90

--- devtools/lexc-giella-style.py
+++ devtools/lexc-giella-style.py
@@ -21,11 +21,16 @@
 
 
 from __future__ import absolute_import, print_function
+
+import argparse
 import codecs
-import unittest
+import io
+import re
 import sys
+import unittest
 from collections import defaultdict
 from io import open
+
 
 class TestLines(unittest.TestCase):
 
@@ -54,10 +59,14 @@ class TestLines(unittest.TestCase):
         self.assertEqual(longest, l.longest)
 
     def test_output_with_empty_upper_lower(self):
-        input = [u' FINAL1         ;\n',
-           u' +N+Sg:             N_ODD_SG       ;\n']
-        expected_result = [u'       FINAL1   ;\n',
-                    u' +N+Sg: N_ODD_SG ;\n']
+        input = [
+            u' FINAL1         ;\n',
+            u' +N+Sg:             N_ODD_SG       ;\n'
+        ]
+        expected_result = [
+            u'       FINAL1   ;\n',
+            u' +N+Sg: N_ODD_SG ;\n'
+        ]
 
         l = Lines()
         l.parse_lines(input)
@@ -65,13 +74,17 @@ class TestLines(unittest.TestCase):
         self.assertEqual(expected_result, l.adjust_lines())
 
     def test_output_with_lexicon_and_semicolon(self):
-        input = [u'LEXICON GOAHTI-NE  !!= * __@CODE@__ Bisyll. V-Nouns; Nominative Sg. and Essive\n',
+        input = [
+            u'LEXICON GOAHTI-NE  !!= * __@CODE@__ Bisyll. V-Nouns\n',
             u' NomV ;\n',
-            u' EssV ;\n']
+            u' EssV ;\n'
+        ]
 
-        expected_result = [u'LEXICON GOAHTI-NE  !!= * __@CODE@__ Bisyll. V-Nouns; Nominative Sg. and Essive\n',
+        expected_result = [
+            u'LEXICON GOAHTI-NE  !!= * __@CODE@__ Bisyll. V-Nouns\n',
             u'  NomV ;\n',
-            u'  EssV ;\n']
+            u'  EssV ;\n'
+        ]
 
         l = Lines()
         l.parse_lines(input)
@@ -125,93 +138,108 @@ class TestLines(unittest.TestCase):
         self.assertEqual(expected_result, l.adjust_lines())
 
     def test_output(self):
-        input = [u'LEXICON DAKTERE\n',
-           u' +N+Sg:             N_ODD_SG       ;\n',
-           u' +N+Pl:             N_ODD_PL       ;\n',
-           u' +N:             N_ODD_ESS      ;\n',
-           u' +N+SgNomCmp:e%^DISIMP    R              ;\n',
-           u' +N+SgGenCmp:e%>%^DISIMPn R              ;\n',
-           u' +N+PlGenCmp:%>%^DISIMPi  R              ;\n',
-           u' +N+Der1+Der/Dimin+N:%»adtj       GIERIEHTSADTJE ;\n',
-           u'+A+Comp+Attr:%>abpa      ATTRCONT    ;  ! båajasabpa,   *båajoesabpa\n',
-           u'   +A:%>X7 NomVadj "good A" ;',
-           u'! Test data:\n',
-           u'!!€gt-norm: daktere # Odd-syllable test\n']
+        input = [
+            u'LEXICON DAKTERE\n',
+            u' +N+Sg:             N_ODD_SG       ;\n',
+            u' +N+Pl:             N_ODD_PL       ;\n',
+            u' +N:             N_ODD_ESS      ;\n',
+            u' +N+SgNomCmp:e%^DISIMP    R              ;\n',
+            u' +N+SgGenCmp:e%>%^DISIMPn R              ;\n',
+            u' +N+PlGenCmp:%>%^DISIMPi  R              ;\n',
+            u' +N+Der1+Der/Dimin+N:%»adtj       GIERIEHTSADTJE ;\n',
+            u'+A+Comp+Attr:%>abpa      ATTRCONT    ;  ! båajasabpa,   '
+            u'*båajoesabpa\n',
+            u'   +A:%>X7 NomVadj "good A" ;',
+            u'! Test data:\n',
+            u'!!€gt-norm: daktere # Odd-syllable test\n'
+        ]
         l = Lines()
         l.parse_lines(input)
 
-        expected_result = [u'LEXICON DAKTERE\n',
-                    u'               +N+Sg:             N_ODD_SG                ;\n',
-                    u'               +N+Pl:             N_ODD_PL                ;\n',
-                    u'                  +N:             N_ODD_ESS               ;\n',
-                    u'         +N+SgNomCmp:e%^DISIMP    R                       ;\n',
-                    u'         +N+SgGenCmp:e%>%^DISIMPn R                       ;\n',
-                    u'         +N+PlGenCmp:%>%^DISIMPi  R                       ;\n',
-                    u' +N+Der1+Der/Dimin+N:%»adtj       GIERIEHTSADTJE          ;\n',
-                    u'        +A+Comp+Attr:%>abpa       ATTRCONT                ; ! båajasabpa,   *båajoesabpa\n',
-                    u'                  +A:%>X7         NomVadj        "good A" ;\n',
-                    u'! Test data:\n',
-                    u'!!€gt-norm: daktere # Odd-syllable test\n']
+        expected_result = [
+            u'LEXICON DAKTERE\n',
+            u'               +N+Sg:             N_ODD_SG                ;\n',
+            u'               +N+Pl:             N_ODD_PL                ;\n',
+            u'                  +N:             N_ODD_ESS               ;\n',
+            u'         +N+SgNomCmp:e%^DISIMP    R                       ;\n',
+            u'         +N+SgGenCmp:e%>%^DISIMPn R                       ;\n',
+            u'         +N+PlGenCmp:%>%^DISIMPi  R                       ;\n',
+            u' +N+Der1+Der/Dimin+N:%»adtj       GIERIEHTSADTJE          ;\n',
+            u'        +A+Comp+Attr:%>abpa       ATTRCONT                ; ! '
+            u'båajasabpa,   *båajoesabpa\n',
+            u'                  +A:%>X7         NomVadj        "good A" ;\n',
+            u'! Test data:\n',
+            u'!!€gt-norm: daktere # Odd-syllable test\n'
+        ]
         self.maxDiff = None
 
         self.assertEqual(expected_result, l.adjust_lines())
+
 
 class TestLine(unittest.TestCase):
 
     def test_line_parser_upper_lower(self):
         input = u'''        +N+SgNomCmp:e%^DISIMP    R              ;'''
-        expected_result = {u'upper': u'+N+SgNomCmp', u'lower': u'e%^DISIMP', u'contlex': u'R', u'comment': u'', u'divisor': u':'}
+        expected_result = {u'upper': u'+N+SgNomCmp', u'lower': u'e%^DISIMP',
+                           u'contlex': u'R', u'comment': u'', u'divisor': u':'}
 
         self.assertEqual(parse_line(input), expected_result)
 
     def test_line_parser_no_lower(self):
         input = u'''               +N+Sg:             N_ODD_SG       ;'''
-        expected_result = {u'upper': u'+N+Sg', u'lower': u'', u'contlex': u'N_ODD_SG', u'comment': u'', u'divisor': u':'}
+        expected_result = {u'upper': u'+N+Sg', u'lower': u'',
+                           u'contlex': u'N_ODD_SG', u'comment': u'',
+                           u'divisor': u':'}
 
         self.assertEqual(parse_line(input), expected_result)
 
     def test_line_parser_no_upper_no_lower(self):
-        input = u''' N_ODD_ESS;''';
+        input = u''' N_ODD_ESS;'''
         expected_result = {u'contlex': u'N_ODD_ESS', u'comment': u''}
 
         self.assertEqual(parse_line(input), expected_result)
 
     def test_line_parser_empty_upper_lower(self):
-        input = u''' : N_ODD_E;''';
-        expected_result = {u'upper': u'', u'lower': u'', u'contlex': u'N_ODD_E', u'comment': u'', u'divisor': u':'}
+        input = u''' : N_ODD_E;'''
+        expected_result = {u'upper': u'', u'lower': u'',
+                           u'contlex': u'N_ODD_E', u'comment': u'',
+                           u'divisor': u':'}
 
         self.assertEqual(parse_line(input), expected_result)
 
     def test_line_parser_with_comment(self):
-        input = u''' +A+Comp+Attr:%>abpa      ATTRCONT    ;  ! båajasabpa,   *båajoesabpa'''
-        expected_result = {u'upper': u'+A+Comp+Attr', u'lower': u'%>abpa', u'contlex': u'ATTRCONT', u'comment': u'! båajasabpa,   *båajoesabpa', u'divisor': u':'}
+        input = u'''+A+Comp+Attr:%>abpa ATTRCONT; ! båajasabpa, *båajoesabpa'''
+        expected_result = {u'upper': u'+A+Comp+Attr', u'lower': u'%>abpa',
+                           u'contlex': u'ATTRCONT',
+                           u'comment': u'! båajasabpa, *båajoesabpa',
+                           u'divisor': u':'}
 
         self.assertEqual(parse_line(input), expected_result)
 
     def test_line_parser_with_translation(self):
         input = u'''  +A:%>X7 NomVadj "good A" ;'''
-        expected_result = {u'upper': u'+A', u'lower': u'%>X7', u'contlex': u'NomVadj', u'translation': u'"good A"', u'comment': u'', u'divisor': u':'}
+        expected_result = {u'upper': u'+A', u'lower': u'%>X7',
+                           u'contlex': u'NomVadj', u'translation':
+                           u'"good A"', u'comment': u'', u'divisor': u':'}
 
         self.assertEqual(parse_line(input), expected_result)
 
     def test_line_parser_with_leading_upper_and_contlex(self):
         input = u'jïh Cc ;'
 
-        expected_result = {u'upper': u'jïh', u'contlex': u'Cc', u'comment': u''}
+        expected_result = {u'upper': u'jïh', u'contlex': u'Cc',
+                           u'comment': u''}
 
         self.assertEqual(parse_line(input), expected_result)
 
     def test_line_parser_with_leading_exclam(self):
         input = u'!dovne Cc ; ! dovne A jïh B'
 
-        expected_result = {u'comment': u'! dovne A jïh B', u'upper': u'dovne', u'contlex': u'Cc', u'exclam': u'!'}
+        expected_result = {u'comment': u'! dovne A jïh B', u'upper': u'dovne',
+                           u'contlex': u'Cc', u'exclam': u'!'}
 
         self.assertEqual(parse_line(input), expected_result)
 
-
-import re
-import io
-import argparse
 
 class Lines(object):
     def __init__(self):
@@ -271,7 +299,7 @@ class Lines(object):
                             (self.longest[u'translation'] -
                              len(l[u'translation']) + 1))
 
-                s.write (u';')
+                s.write(u';')
 
                 if l[u'comment'] != u'':
                     s.write(u' ')
@@ -284,6 +312,7 @@ class Lines(object):
 
         return newlines
 
+
 def parse_line(line):
     line_dict = defaultdict(unicode)
 
@@ -293,12 +322,16 @@ def parse_line(line):
         line = commentre.sub('', line)
         line_dict[u'exclam'] = u'!'
 
-    contlexre = re.compile(ur'(?P<contlex>\S+)(?P<translation>\s+".+")*\s*;\s*(?P<comment>.*)')
+    contlexre = re.compile(ur'(?P<contlex>\S+)'
+                           ur'(?P<translation>\s+".+")'
+                           ur'*\s*;\s*'
+                           ur'(?P<comment>.*)')
     m = contlexre.search(line)
 
     line_dict[u'contlex'] = contlexre.search(line).group(u'contlex')
     if m.group(u'translation'):
-        line_dict[u'translation'] = contlexre.search(line).group(u'translation').strip()
+        line_dict[u'translation'] = contlexre.search(
+            line).group(u'translation').strip()
     line_dict[u'comment'] = contlexre.search(line).group(u'comment').strip()
 
     line = contlexre.sub(u'', line)
@@ -315,9 +348,14 @@ def parse_line(line):
 
     return line_dict
 
+
 def parse_options():
-    parser = argparse.ArgumentParser(description = u'Align rules given in lexc files')
-    parser.add_argument(u'lexcfile', help = u'Lexc file where rules should be aligned\nIf filename is -, then the file is read from stdin and written to stdout.')
+    parser = argparse.ArgumentParser(
+        description=u'Align rules given in lexc files')
+    parser.add_argument(u'lexcfile',
+                        help=u'Lexc file where rules should be aligned\n'
+                        'If filename is -, then the file is read from '
+                        'stdin and written to stdout.')
 
     args = parser.parse_args()
     return args

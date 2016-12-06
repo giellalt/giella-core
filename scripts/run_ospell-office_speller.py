fc7b0a8b33286eea9e1e27c6aa22a4cec0961a66
boerre <boerre@c7155fb1-f0a7-4240-a2fc-2600b6f42f90>
2016-12-06 14:25:59 +0000

    Remove doubled help text
    git-svn-id: https://gtsvn.uit.no/langtech/trunk/giella-core@144950 c7155fb1-f0a7-4240-a2fc-2600b6f42f90

--- scripts/run_ospell-office_speller.py
+++ scripts/run_ospell-office_speller.py
@@ -15,22 +15,6 @@ Runs the command line speller 'voikkospell' with input from INFILE,
 writing the result to OUTFILE for the language given by LANGCODE,
 optionally using the zhfst file found in DICTDIR.
 
-Usage: $0 INFILE OUTFILE TIMEUSE LANGCODE [DICTDIR]
-
-INFILE   = list of potential misspellings, one word pr line
-OUTFILE  = output from speller
-TIMEUSE  = filename for storing the elapsed time for the speller run
-LANGCODE = ISO 639-1, -2 or -3 language code for the language to test
-           Please use the 639-1 code if there is one.
-DICTDIR  = directory containing a subdir 3/ containin a zhfst file.
-           Optional. If not specified, speller files from the default
-           directories will be used.
-
-
-Runs the command line speller 'voikkospell' with input from INFILE,
-writing the result to OUTFILE for the language given by LANGCODE,
-optionally using the zhfst file found in DICTDIR.
-
 Usage: {} INFILE OUTFILE TIMEUSE LANGCODE [DICTDIR]
 
 INFILE   = list of potential misspellings, one word pr line

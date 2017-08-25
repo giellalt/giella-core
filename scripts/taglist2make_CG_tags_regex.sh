c9ec486c98bcc62e2a7d61cab0aff788f64f46ab
sjur <sjur@c7155fb1-f0a7-4240-a2fc-2600b6f42f90>
2017-08-25 09:42:16 +0000

    Another bug fix: the sed scripts to convert tags from + format to CG format (spaces instead of pluses) didn’t work well with GNU sed, and created a garbage regex. This version has been tested against both BSD and GNU, and the escaping of + characters should be much more robust and portable. Increased version number.
    git-svn-id: https://gtsvn.uit.no/langtech/trunk/giella-core@156229 c7155fb1-f0a7-4240-a2fc-2600b6f42f90

--- scripts/taglist2make_CG_tags_regex.sh
+++ scripts/taglist2make_CG_tags_regex.sh
@@ -9,12 +9,14 @@ SED=sed
 AWK=awk
 
 # Script explanation:
-# 1. the first line converts suffix tags
-# 2. the second line converts prefix tags
-# 3. the third line replaces the last comma with a semicolon, to end the regex
+# 1. the first line converts tags with plusses in both ends
+# 1. the second line converts suffix tags
+# 2. the third line converts prefix tags
+# 3. the fourth line replaces the last comma with a semicolon, to end the regex
 # 4. finally, a warning comment is printed at the top of the file
 
-$SED   's/^\+\(.*\)$/" \1" <- "+\1",/' $1 \
-| $SED 's/^\(.*\)\+$/"\1 " <- "\1+",/' \
+$SED '/^[+][^+]*[+]$/s/^[+]\([^+]*\)[+]$/\" \1\ " <- \"+\1\+",/' $1 \
+| $SED '/^[+][^+]*$/s/^[+]\([^+]*\)$/\" \1\" <- \"+\1\",/' \
+| $SED '/^[^+]*[+]$/s/^\([^+]*\)[+]$/\"\1 \" <- \"\1+\",/' \
 | $SED '$ s/,/;/' \
 | $AWK 'NR==1{$0="### This is a file  generated with taglist2make_CG_tags_regex.sh - do not edit!!!\n\n"$0}1'

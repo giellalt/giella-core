291aac82bce4ffd69db3d36c482625687938fbfb
sjur <sjur@c7155fb1-f0a7-4240-a2fc-2600b6f42f90>
2018-01-17 10:00:06 +0000

    Improved/fixed the shell script that converts regular Giella tags to CG friendly tags to be used with the tokeniser, so that it now checks that all tags follow the basic conventions (+TAG, TAG+ or +TAG+), and if not, errors out and lists all non-conformant tags. That is, it now explains why it fails, and list all the information to fix the problem.
    git-svn-id: https://gtsvn.uit.no/langtech/trunk/giella-core@162313 c7155fb1-f0a7-4240-a2fc-2600b6f42f90

--- scripts/taglist2make_CG_tags_regex.sh
+++ scripts/taglist2make_CG_tags_regex.sh
@@ -8,15 +8,28 @@
 SED=sed
 AWK=awk
 
-# Script explanation:
+# SED/AWK script explanation:
 # 1. the first line converts tags with plusses in both ends
 # 1. the second line converts suffix tags
 # 2. the third line converts prefix tags
 # 3. the fourth line replaces the last comma with a semicolon, to end the regex
 # 4. finally, a warning comment is printed at the top of the file
 
-$SED '/^[+][^+]*[+]$/s/^[+]\([^+]*\)[+]$/\" \1\ " <- \"+\1\+",/' $1 \
-| $SED '/^[+][^+]*$/s/^[+]\([^+]*\)$/\" \1\" <- \"+\1\",/' \
-| $SED '/^[^+]*[+]$/s/^\([^+]*\)[+]$/\"\1 \" <- \"\1+\",/' \
-| $SED '$ s/,/;/' \
-| $AWK 'NR==1{$0="### This is a file  generated with taglist2make_CG_tags_regex.sh - do not edit!!!\n\n"$0}1'
+# But first, check that all tags conform, and if not print an error message and
+# the list of non-conformant tags:
+if $( egrep -v -q '(^\+[^\+]+$|^[^\+]+\+$|^\+[^\+]+\+$)' $1 ) ; then
+  echo "ERROR:" 1>&2 ;
+  echo "The following tags do not follow the Giella tag conventions" 1>&2 ;
+  echo '(either +TAG, TAG+ or +TAG+) and can not be converted to the CG' 1>&2 ;
+  echo "format required for proper tokeniser functionality within the" 1>&2 ;
+  echo "Giella infrastructure:" 1>&2 ;
+  echo  1>&2 ;
+  egrep -v '(^\+[^\+]+$|^[^\+]+\+$|^\+[^\+]+\+$)' $1 1>&2 ;
+  exit 1;
+else
+  $SED '/^[+][^+]*[+]$/s/^[+]\([^+]*\)[+]$/\" \1\ " <- \"+\1\+",/' $1 \
+  | $SED '/^[+][^+]*$/s/^[+]\([^+]*\)$/\" \1\" <- \"+\1\",/' \
+  | $SED '/^[^+]*[+]$/s/^\([^+]*\)[+]$/\"\1 \" <- \"\1+\",/' \
+  | $SED '$ s/,/;/' \
+  | $AWK 'NR==1{$0="### This is a file  generated with taglist2make_CG_tags_regex.sh - do not edit!!!\n\n"$0}1' ;
+fi

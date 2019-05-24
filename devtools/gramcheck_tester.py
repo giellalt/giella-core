7cd2537ae7748d99bee253958937b2ce4a9a9091
boerre <boerre@c7155fb1-f0a7-4240-a2fc-2600b6f42f90>
2019-05-24 09:32:07 +0000

    Write results out as a table
    git-svn-id: https://gtsvn.uit.no/langtech/trunk/giella-core@180003 c7155fb1-f0a7-4240-a2fc-2600b6f42f90

--- devtools/gramcheck_tester.py
+++ devtools/gramcheck_tester.py
@@ -23,41 +23,30 @@ import json
 import os
 import sys
 
+from lxml import etree
+
 from corpustools import util
 
 
-def getpath(corpus_lang: str) -> str:
+def getpath(lang: str) -> str:
     """Get the corpus path."""
     gtfree = str(os.getenv('GTFREE'))
 
-    return os.path.join(gtfree, 'goldstandard/converted', corpus_lang)
+    return os.path.join(gtfree, 'goldstandard/converted', lang)
 
 
-def get_error_sentences(corpus_lang: str,
-                        runner: util.ExternalCommandRunner) -> str:
+def get_error_sentences(lang: str, runner: util.ExternalCommandRunner) -> str:
     """Get the sentences containing errors from the corpus."""
     runner = util.ExternalCommandRunner()
-    runner.run(f'ccat -a -l {corpus_lang} {getpath(corpus_lang)}'.split())
+    runner.run(f'ccat -a -l {lang} {getpath(lang)}'.split())
     return runner.stdout.decode('utf-8')
 
 
-def get_correct_sentences(corpus_lang: str,
+def get_correct_sentences(lang: str,
                           runner: util.ExternalCommandRunner) -> str:
     """Get the corrected sentences from the corpus."""
-    runner.run(f'ccat -c -a -l {corpus_lang} {getpath(corpus_lang)}'.split())
+    runner.run(f'ccat -c -a -l {lang} {getpath(lang)}'.split())
     return runner.stdout.decode('utf-8')
-
-
-def correct(error: list, sentence: str) -> str:
-    """Replace the given error with the first correction."""
-    print(error)
-    start = error[1]
-    fixes = error[5]
-    end = error[2]
-    if fixes:
-        return ''.join([sentence[:start], fixes[0], sentence[end:]])
-
-    return sentence
 
 
 def gramcheck(sentence: str, corpus_lang: str,
@@ -68,56 +57,229 @@ def gramcheck(sentence: str, corpus_lang: str,
         f'divvun-checker -l {three2two[corpus_lang]} -n smegram'.split(),
         to_stdin=sentence.encode('utf-8'))
 
-    output = json.loads(runner.stdout)
-    if output['errs']:
-        output['errs'] = [error for error in output['errs'] if error[5]]
-
-    return output
+    return json.loads(runner.stdout)
 
 
-def get_gramcheck_results(sentence: str, errors: dict) -> str:
-    """Replace errors with corrections from the error dict."""
-    corrected_sentence = sentence
-    for error in reversed(errors):
-        corrected_sentence = correct(error, corrected_sentence)
+def make_error_parts(grammarcheck_result: dict) -> list:
+    """Make a list out of a grammarchecker error dict.
 
-    return corrected_sentence
+    This list will later be used to construct strings for the report.
+    """
+    hint: list = []
+    previous_end: int = 0
+    text: str = grammarcheck_result['text']
+    for error in grammarcheck_result['errs']:
+        hint.append(text[previous_end:error[1]])
+        hint.append([error[0], error[5] if error[5] else ['']])
+        previous_end = error[2]
+    hint.append(text[previous_end:])
+
+    return hint
 
 
-for lang in sys.argv[1:]:
-    command_runner = util.ExternalCommandRunner()
-    error_sentences = [
-        sentence
-        for sentence in get_error_sentences(lang, command_runner).split(u'\n')
-        if sentence.strip()
-    ]
-    correct_sentences = [
-        sentence for sentence in get_correct_sentences(lang, command_runner).
-        split(u'\n') if sentence.strip()
-    ]
+def make_orig(error_parts: list) -> str:
+    """Make a original sentence.
 
-    err_len = len(error_sentences)
-    corr_len = len(correct_sentences)
-    if err_len != corr_len:
-        raise SystemExit(f'erroneous input. err: {err_len}, corr: {corr_len}')
+    Mark it up with error classes.
+    """
+    orig = ['<td>']
 
-    for sentence_no, error_sentence in enumerate(error_sentences):
-        if sentence_no < 50:
-            if error_sentence.strip():
-                check_output = gramcheck(error_sentence, lang, command_runner)
-                corrected = check_output['text']
-                gramcheck_runs = 0
-                while check_output['errs'] and gramcheck_runs < 10:
-                    to_correct = corrected
-                    corrected = get_gramcheck_results(to_correct,
-                                                      check_output['errs'])
-                    check_output = gramcheck(corrected, lang, command_runner)
-                    gramcheck_runs += 1
-                if gramcheck_runs:  # to only print those with errors
-                    print(f'errorsentence\t{sentence_no} {gramcheck_runs} '
-                          f'{error_sentence}')
-                    print(f'gramcheck\t{sentence_no} {gramcheck_runs} '
-                          f'{corrected}')
-                    print(f'ccat facit\t{sentence_no} {gramcheck_runs} '
-                          f'{correct_sentences[sentence_no]}')
-                    print()
+    error_count = 0
+    for error_part in error_parts:
+        if isinstance(error_part, str):
+            orig.append(error_part)
+        else:
+            orig.append(
+                f'<span class="error{error_count}">{error_part[0]}</span>')
+            error_count += 1
+    orig.append('</td>')
+
+    return ''.join(orig)
+
+
+def make_corrected(error_parts: list, for_web: bool = False) -> str:
+    """Make a corrected sentence.
+
+    If web ready, mark it up with error classes.
+    """
+    orig = []
+    if for_web:
+        orig.append('<td>')
+    error_count = 0
+    for error_part in error_parts:
+        if isinstance(error_part, str):
+            orig.append(error_part)
+        else:
+            if for_web:
+                orig.append(f'<span class="error{error_count}">')
+            if error_part[1][0].strip():
+                orig.append(error_part[1][0])
+            else:
+                orig.append('😱')
+            if for_web:
+                orig.append('</span>')
+            error_count += 1
+
+    if for_web:
+        orig.append('</td>')
+
+    return ''.join(orig)
+
+
+def make_error(error_parts: list) -> str:
+    """Make an error list.
+
+    It has the same class as the errors.
+    """
+    table_d = etree.Element('td')
+    unordered_list = etree.SubElement(table_d, 'ul')
+    for errno, error in enumerate([
+            error_part for error_part in error_parts
+            if not isinstance(error_part, str)
+    ]):
+        errors = []
+        errors.append(f'<li><span class="error{errno}">«')
+        errors.append(error[0])
+        errors.append('» ➞ «')
+        errors.append(', '.join(error[1]))
+        errors.append('»</span></li>')
+        unordered_list.append(etree.fromstring(''.join(errors)))
+
+    return table_d
+
+
+def make_error_parts_table(error_parts_list: list):
+    """Make the sub table to display errors."""
+    error_table = etree.Element('table')
+    error_table_head = etree.SubElement(error_table, 'thead')
+    first_tr = etree.SubElement(error_table_head, 'tr')
+    for header in ['error sentence', 'corrections', 'errors']:
+        thead = etree.SubElement(first_tr, 'th')
+        thead.text = header
+
+    main_table_tbody = etree.SubElement(error_table, 'tbody')
+
+    for error_parts in error_parts_list:
+        table_row = etree.SubElement(main_table_tbody, 'tr')
+        table_row.append(etree.fromstring(make_orig(error_parts)))
+        table_row.append(
+            etree.fromstring(make_corrected(error_parts, for_web=True)))
+        table_row.append(make_error(error_parts))
+
+    return error_table
+
+
+def make_gramcheck_runs(error_sentence: str, lang: str,
+                        runner: util.ExternalCommandRunner) -> list:
+    """Turn error_sentence and grammar checks to a list."""
+    gramcheck_dict: dict = gramcheck(error_sentence, lang, runner)
+    error_parts_list: list = []
+
+    gramcheck_runs = 0
+    while gramcheck_dict['errs'] and gramcheck_runs < 10:
+        error_parts = make_error_parts(gramcheck_dict)
+        error_parts_list.append(error_parts)
+        gramcheck_runs += 1
+        gramcheck_dict = gramcheck(make_corrected(error_parts), lang, runner)
+
+    return error_parts_list
+
+
+def make_table(error_data_list: list):
+    """Make the main table that displays all error data."""
+    main_table = etree.Element('table')
+    main_table.set('id', 'results')
+    main_table_head = etree.SubElement(main_table, 'thead')
+    first_tr = etree.SubElement(main_table_head, 'tr')
+    for header in [
+            'orig sentence', 'corrected sentence', 'runs', 'corrections'
+    ]:
+        thead = etree.SubElement(first_tr, 'th')
+        thead.text = header
+
+    main_table_tbody = etree.SubElement(main_table, 'tbody')
+    for error_data in error_data_list:
+        table_row = etree.SubElement(main_table_tbody, 'tr')
+        td1 = etree.SubElement(table_row, 'td')
+        td1.text = error_data[0]
+        td2 = etree.SubElement(table_row, 'td')
+        td2.text = error_data[1]
+        td3 = etree.SubElement(table_row, 'td')
+        td3.text = str(len(error_data[2]) if error_data[2] else 1)
+        td4 = etree.SubElement(table_row, 'td')
+        if error_data[2]:
+            td4.append(make_error_parts_table(error_data[2]))
+        else:
+            td4.text = 'No errors found'
+
+    return main_table
+
+
+def make_html():
+    """Make the head of the grammarcheck_result html file."""
+    html = etree.Element('html')
+    html.append(
+        etree.fromstring('''
+        <head>
+            <meta charset="UTF-8"/>
+            <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js" integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8=" crossorigin="anonymous"> </script>
+            <script src="https://cdnjs.cloudflare.com/ajax/libs/Dynatable/0.3.1/jquery.dynatable.min.js" integrity="sha256-/kLSC4kLFkslkJlaTgB7TjurN5TIcmWfMfaXyB6dVh0=" crossorigin="anonymous"> </script>
+            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/Dynatable/0.3.1/jquery.dynatable.min.css" integrity="sha256-lxcbK1S14B8LMgrEir2lv2akbdyYwD1FwMhFgh2ihls=" crossorigin="anonymous"/>
+            <link rel="stylesheet" href="https://gtsvn.uit.no/langtech/trunk/giella-core/scripts/style/gramcheck.css"/>
+        </head>
+    '''))
+
+    return html
+
+
+def main():
+    """The main routine of the grammarcheck result script."""
+    for lang in sys.argv[1:]:
+        command_runner = util.ExternalCommandRunner()
+        error_sentences = [
+            sentence for sentence in get_error_sentences(lang, command_runner).
+            split(u'\n') if sentence.strip()
+        ]
+        correct_sentences = [
+            sentence for sentence in get_correct_sentences(
+                lang, command_runner).split(u'\n') if sentence.strip()
+        ]
+
+        err_len = len(error_sentences)
+        corr_len = len(correct_sentences)
+        if err_len != corr_len:
+            raise SystemExit(
+                f'erroneous input. err: {err_len}, corr: {corr_len}')
+
+        huff = [(error_sentence, correct_sentence)
+                for error_sentence, correct_sentence in zip(
+                    error_sentences, correct_sentences)]
+
+        error_data_list = [
+            (error_sentence, correct_sentence,
+             make_gramcheck_runs(error_sentence, lang, command_runner))
+            for error_sentence, correct_sentence in huff[2440:]
+            if error_sentence.strip()
+            and not ('Nu fal. Nu dehalaš' in error_sentence
+                     or 'Okta joavku mas čuojahan' in error_sentence)
+        ]
+
+        html = make_html()
+        body = etree.SubElement(html, 'body')
+        body.append(make_table(error_data_list))
+        body.append(
+            etree.fromstring('''
+            <script
+                src="https://gtsvn.uit.no/langtech/trunk/giella-core/scripts/javascript/tablesorter.js">
+            </script>'''))
+
+        with open('/tmp/tmp1.html', 'wb') as tmp1:
+            tmp1.write(
+                etree.tostring(
+                    html,
+                    pretty_print=True,
+                    encoding='utf-8',
+                    xml_declaration=True))
+
+
+main()

01ed80adba7e24240eb0056007a8cdf25ad5ace1
boerre <boerre@c7155fb1-f0a7-4240-a2fc-2600b6f42f90>
2019-09-16 15:17:59 +0000

    More statistics
    git-svn-id: https://gtsvn.uit.no/langtech/trunk/giella-core@183058 c7155fb1-f0a7-4240-a2fc-2600b6f42f90

--- devtools/gramcheck_comparator.py
+++ devtools/gramcheck_comparator.py
@@ -1,14 +1,16 @@
 # -*- coding:utf-8 -*-
 
 import pickle
+from collections import defaultdict
 
 
 def correct_not_in_dc(correct, dc):
     nices = [
         (c_error['error'], c_error['correct'], d_error[5])
-        for c_error in correct
-        for d_error in dc
-        if (c_error['start'] == d_error[1] and c_error['end'] == d_error[2]) and (c_error['error'] == d_error[0] and c_error['correct'] not in d_error[5])
+        for c_error in correct for d_error in dc
+        if (c_error['start'] == d_error[1] and c_error['end'] == d_error[2])
+        and (c_error['error'] == d_error[0]
+             and c_error['correct'] not in d_error[5])
     ]
 
     if nices:
@@ -17,13 +19,17 @@ def correct_not_in_dc(correct, dc):
         for nice in nices:
             print(f'\t\t{nice[0]} -> {nice[1]}, dc-suggestions {nice[2]}')
 
+    return len(nices)
+
 
 def correct_in_dc(correct, dc):
     nices = [
-        (c_error['error'], c_error['correct'], d_error[5].index(c_error['correct']))
-        for c_error in correct
+        (c_error['error'], c_error['correct'],
+         d_error[5].index(c_error['correct'])) for c_error in correct
         for d_error in dc
-        if (c_error['start'] == d_error[1] and c_error['end'] == d_error[2] and d_error[5]) and (c_error['error'] == d_error[0] and c_error['correct'] in d_error[5])
+        if (c_error['start'] == d_error[1] and c_error['end'] == d_error[2]
+            and d_error[5]) and
+        (c_error['error'] == d_error[0] and c_error['correct'] in d_error[5])
     ]
 
     if nices:
@@ -31,6 +37,8 @@ def correct_in_dc(correct, dc):
         print('\tcorrect and dc align, and correction found in dc')
         for nice in nices:
             print(f'\t\t{nice[0]} -> {nice[1]}, position {nice[2]}')
+
+    return len(nices)
 
 
 def corrects_not_in_dc(correct, dc):
@@ -48,6 +56,8 @@ def corrects_not_in_dc(correct, dc):
         for c_error in corrects:
             print(f'\t\t{c_error}')
 
+    return len(corrects)
+
 
 def dcs_not_in_correct(correct, dc):
     corrects = []
@@ -64,17 +74,31 @@ def dcs_not_in_correct(correct, dc):
         for c_error in corrects:
             print(f'\t\t{c_error}')
 
+    return len(corrects)
+
 
 with open('results.pickle', 'rb') as pickle_stream:
     results = pickle.load(pickle_stream)
 
 print(f'Paragraphs {len(results)}')
+counter = defaultdict(int)
 for result in results:
     if result[1] or result[3]["errs"]:
+        counter['total_manually_marked_errors'] += len(result[1])
+        for manual in result[1]:
+            counter[f'manually_marked_errors_{manual["type"]}'] += 1
+        counter['total_grammarchecker_errors'] += len(result[3]["errs"])
+        for dc_error in result[3]["errs"]:
+            counter[f'grammarchecker_errors_{dc_error[3].replace(" ", "_")}'] += 1
         print('==========')
         print(result[0])
-        corrects_not_in_dc(result[1], result[3]["errs"])
-        dcs_not_in_correct(result[1], result[3]["errs"])
-        correct_in_dc(result[1], result[3]["errs"])
-        correct_not_in_dc(result[1], result[3]["errs"])
+        counter['total_manual_errors_not_found_by_grammarchecker'] += corrects_not_in_dc(result[1],
+                                                     result[3]["errs"])
+        counter['total_grammarchecker_errors_not_found_in_manual_markup'] += dcs_not_in_correct(result[1], result[3]["errs"])
+        counter['total_manual_and_grammarchecker_align_correction_with_suggestion'] += correct_in_dc(result[1], result[3]["errs"])
+        counter['total_manual_and_grammarchecker_align_correction_without_suggestion'] += correct_not_in_dc(
+            result[1], result[3]["errs"])
         print()
+
+for label in sorted(counter):
+    print(f'{label}: {counter[label]}')

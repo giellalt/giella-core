b30b92443ad7eb2d35559076cbbc11c5d73f2dcb
boerre <boerre@c7155fb1-f0a7-4240-a2fc-2600b6f42f90>
2019-09-23 09:27:32 +0000

    Send the username as a variable
    git-svn-id: https://gtsvn.uit.no/langtech/trunk/giella-core@183322 c7155fb1-f0a7-4240-a2fc-2600b6f42f90

--- devtools/gramchecker_tester_update_corpus.sh
+++ devtools/gramchecker_tester_update_corpus.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
+# usage: gramchecker_tester_update_corpus.sh thomas
+
 cd $GTFREE
 echo "Updating $GTFREE"
 rm -rf correct-no-gs
 rm -rf goldstandard
 svn -q up
-svn log -v -r7534:HEAD | awk '/^r[0-9]+ / {user=$3} /./ {if (user=="thomas") {print}}'| grep -E "^   M|^   G|^   A|^   D|^   C|^   U" | awk '{print $2}'|sort|uniq|fgrep /orig/sme | sed 's_/orig/_orig/_' | xargs convert2xml --goldstandard
+svn log -v -r7534:HEAD | awk -v username="$1" '{print username} /^r[0-9]+ / {user=$3} /./ {if (user==username) {print}}'| grep -E "^   M|^   G|^   A|^   D|^   C|^   U" | awk '{print $2}'|sort|uniq|fgrep /orig/sme | sed 's_/orig/_orig/_' | xargs convert2xml --goldstandard
 
 cd $GTBOUND
 echo "Updating $GTBOUND"
 rm -rf correct-no-gs
 rm -rf goldstandard
 svn -q up
-svn log -v -r5037:HEAD | awk '/^r[0-9]+ / {user=$3} /./ {if (user=="thomas") {print}}'| grep -E "^   M|^   G|^   A|^   D|^   C|^   U" | awk '{print $2}'|sort|uniq|fgrep /orig/sme | sed 's_/orig/_orig/_' | xargs convert2xml --goldstandard
+svn log -v -r5037:HEAD | awk -v username="$1" '{print username} /^r[0-9]+ / {user=$3} /./ {if (user==username) {print}}'| grep -E "^   M|^   G|^   A|^   D|^   C|^   U" | awk '{print $2}'|sort|uniq|fgrep /orig/sme | sed 's_/orig/_orig/_' | xargs convert2xml --goldstandard

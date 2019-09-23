cefd7191d692a6b67909f28b1f069f67832a1b68
boerre <boerre@c7155fb1-f0a7-4240-a2fc-2600b6f42f90>
2019-09-23 09:27:49 +0000

    Tell that conversion has ended
    git-svn-id: https://gtsvn.uit.no/langtech/trunk/giella-core@183326 c7155fb1-f0a7-4240-a2fc-2600b6f42f90

--- devtools/gramchecker_tester_update_corpus.sh
+++ devtools/gramchecker_tester_update_corpus.sh
@@ -14,3 +14,5 @@ rm -rf correct-no-gs
 rm -rf goldstandard
 svn -q up
 svn log -v -r5037:HEAD | awk -v username="$1" '{print username} /^r[0-9]+ / {user=$3} /./ {if (user==username) {print}}'| grep -E "^   M|^   G|^   A|^   D|^   C|^   U" | awk '{print $2}'|sort|uniq|fgrep /orig/sme | sed 's_/orig/_orig/_' | xargs convert2xml --goldstandard
+
+echo "Finished with conversion"

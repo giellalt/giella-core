9997218ab093e409408e7d3d7492170fb351d57f
Trond Trosterud <trond.trosterud@uit.no>
2020-05-21 17:27:25 +0200

    Scripts for scrolling through all catalogues and updating them. TODO: Ad an if statement to check whether the catalogue is there.

---
+++ scripts/git_pull_all.sh
@@ -0,0 +1,202 @@
+echo "\nHentar frå aka (Akan)"
+cd ~/git/giellalt/lang-aka && git pull
+echo "\nHentar frå amh (Amharic)"
+cd ~/git/giellalt/lang-amh && git pull
+echo "\nHentar frå apu (Apurinã)"
+cd ~/git/giellalt/lang-apu && git pull
+echo "\nHentar frå aym (Aymara)"
+cd ~/git/giellalt/lang-aym && git pull
+echo "\nHentar frå bak (Bashkir)"
+cd ~/git/giellalt/lang-bak && git pull
+echo "\nHentar frå bla (Siksika)"
+cd ~/git/giellalt/lang-bla && git pull
+echo "\nHentar frå chp (Chipewyan)"
+cd ~/git/giellalt/lang-chp && git pull
+echo "\nHentar frå chr (Cherokee)"
+cd ~/git/giellalt/lang-chr && git pull
+echo "\nHentar frå ciw (Chippewa)"
+cd ~/git/giellalt/lang-ciw && git pull
+echo "\nHentar frå ckt (Chukot)"
+cd ~/git/giellalt/lang-ckt && git pull
+echo "\nHentar frå cor (Cornish)"
+cd ~/git/giellalt/lang-cor && git pull
+echo "\nHentar frå crj (Southern East Cree)"
+cd ~/git/giellalt/lang-crj && git pull
+echo "\nHentar frå crk (Plains Cree)"
+cd ~/git/giellalt/lang-crk && git pull
+echo "\nHentar frå crl (Northern East Cree)"
+cd ~/git/giellalt/lang-crl && git pull
+echo "\nHentar frå deu (German)"
+cd ~/git/giellalt/lang-deu && git pull
+echo "\nHentar frå dgr (Dogrib)"
+cd ~/git/giellalt/lang-dgr && git pull
+echo "\nHentar frå eng (English)"
+cd ~/git/giellalt/lang-eng && git pull
+echo "\nHentar frå epo (Esperanto)"
+cd ~/git/giellalt/lang-epo && git pull
+echo "\nHentar frå ess (Central Siberian Yupik)"
+cd ~/git/giellalt/lang-ess && git pull
+echo "\nHentar frå eus (Basque)"
+cd ~/git/giellalt/lang-eus && git pull
+echo "\nHentar frå evn (Evenki)"
+cd ~/git/giellalt/lang-evn && git pull
+echo "\nHentar frå fao (Faroese)"
+cd ~/git/giellalt/lang-fao && git pull
+echo "\nHentar frå fin (Finnish)"
+cd ~/git/giellalt/lang-fin && git pull
+echo "\nHentar frå fit (Tornedalen Finnish)"
+cd ~/git/giellalt/lang-fit && git pull
+echo "\nHentar frå fkv (Kven Finnish)"
+cd ~/git/giellalt/lang-fkv && git pull
+echo "\nHentar frå gle (Irish)"
+cd ~/git/giellalt/lang-gle && git pull
+echo "\nHentar frå grn (Guarani)"
+cd ~/git/giellalt/lang-grn && git pull
+echo "\nHentar frå hdn (Northern Haida)"
+cd ~/git/giellalt/lang-hdn && git pull
+echo "\nHentar frå hin (Hindi)"
+cd ~/git/giellalt/lang-hin && git pull
+echo "\nHentar frå hun (Hungarian)"
+cd ~/git/giellalt/lang-hun && git pull
+echo "\nHentar frå iku (Inuktitut)"
+cd ~/git/giellalt/lang-iku && git pull
+echo "\nHentar frå ipk (Inupiaq)"
+cd ~/git/giellalt/lang-ipk && git pull
+echo "\nHentar frå izh (Ingrian)"
+cd ~/git/giellalt/lang-izh && git pull
+echo "\nHentar frå kal (Kalaallisut)"
+cd ~/git/giellalt/lang-kal && git pull
+echo "\nHentar frå kca (Khanty)"
+cd ~/git/giellalt/lang-kca && git pull
+echo "\nHentar frå kek (Kekchí)"
+cd ~/git/giellalt/lang-kek && git pull
+echo "\nHentar frå khk (Halh Mongolian)"
+cd ~/git/giellalt/lang-khk && git pull
+echo "\nHentar frå kio (Kiowa)"
+cd ~/git/giellalt/lang-kio && git pull
+echo "\nHentar frå kjh (Khakas)"
+cd ~/git/giellalt/lang-kjh && git pull
+echo "\nHentar frå kmr (Northern Kurdish)"
+cd ~/git/giellalt/lang-kmr && git pull
+echo "\nHentar frå koi (Komi-Permyak)"
+cd ~/git/giellalt/lang-koi && git pull
+echo "\nHentar frå kpv (Komi-Zyrian)"
+cd ~/git/giellalt/lang-kpv && git pull
+echo "\nHentar frå krl (Karelian)"
+cd ~/git/giellalt/lang-krl && git pull
+echo "\nHentar frå lav (Latvian)"
+cd ~/git/giellalt/lang-lav && git pull
+echo "\nHentar frå liv (Liv)"
+cd ~/git/giellalt/lang-liv && git pull
+echo "\nHentar frå luo (Luo in Kenya and Tanzania)"
+cd ~/git/giellalt/lang-luo && git pull
+echo "\nHentar frå lut (Lushootseed)"
+cd ~/git/giellalt/lang-lut && git pull
+echo "\nHentar frå mdf (Moksha)"
+cd ~/git/giellalt/lang-mdf && git pull
+echo "\nHentar frå mhr (Eastern Mari)"
+cd ~/git/giellalt/lang-mhr && git pull
+echo "\nHentar frå mns (Mansi)"
+cd ~/git/giellalt/lang-mns && git pull
+echo "\nHentar frå moe (Montagnais)"
+cd ~/git/giellalt/lang-moe && git pull
+echo "\nHentar frå moh (Mohawk)"
+cd ~/git/giellalt/lang-moh && git pull
+echo "\nHentar frå mrj (Western Mari)"
+cd ~/git/giellalt/lang-mrj && git pull
+echo "\nHentar frå myv (Erzya)"
+cd ~/git/giellalt/lang-myv && git pull
+echo "\nHentar frå ndl (Ndolo)"
+cd ~/git/giellalt/lang-ndl && git pull
+echo "\nHentar frå nds (Low German)"
+cd ~/git/giellalt/lang-nds && git pull
+echo "\nHentar frå nio (Nganasan)"
+cd ~/git/giellalt/lang-nio && git pull
+echo "\nHentar frå nno (Norwegian Nynorsk)"
+cd ~/git/giellalt/lang-nno && git pull
+echo "\nHentar frå nob (Norwegian Bokmål)"
+cd ~/git/giellalt/lang-nob && git pull
+echo "\nHentar frå non (Old Norse)"
+cd ~/git/giellalt/lang-non && git pull
+echo "\nHentar frå nso (Pedi)"
+cd ~/git/giellalt/lang-nso && git pull
+echo "\nHentar frå oji (Ojibwa)"
+cd ~/git/giellalt/lang-oji && git pull
+echo "\nHentar frå olo (Livvi)"
+cd ~/git/giellalt/lang-olo && git pull
+echo "\nHentar frå otw (Ottawa)"
+cd ~/git/giellalt/lang-otw && git pull
+echo "\nHentar frå rmf (Kalo Finnish Romani)"
+cd ~/git/giellalt/lang-rmf && git pull
+echo "\nHentar frå rmn (Balkan Romani)"
+cd ~/git/giellalt/lang-rmn && git pull
+echo "\nHentar frå rmu (Tavringer Romani)"
+cd ~/git/giellalt/lang-rmu && git pull
+echo "\nHentar frå rmy (Vlax Romani)"
+cd ~/git/giellalt/lang-rmy && git pull
+echo "\nHentar frå ron (Romanian)"
+cd ~/git/giellalt/lang-ron && git pull
+echo "\nHentar frå rup (Macedo-Romanian)"
+cd ~/git/giellalt/lang-rup && git pull
+echo "\nHentar frå rus (Russian)"
+cd ~/git/giellalt/lang-rus && git pull
+echo "\nHentar frå sel (Selkup)"
+cd ~/git/giellalt/lang-sel && git pull
+echo "\nHentar frå sjd (Kildin Sami)"
+cd ~/git/giellalt/lang-sjd && git pull
+echo "\nHentar frå sje (Pite Sami)"
+cd ~/git/giellalt/lang-sje && git pull
+echo "\nHentar frå skf (Sakirabiá)"
+cd ~/git/giellalt/lang-skf && git pull
+echo "\nHentar frå sma (Southern Sami)"
+cd ~/git/giellalt/lang-sma && git pull
+echo "\nHentar frå sme (Northern Sami)"
+cd ~/git/giellalt/lang-sme && git pull
+echo "\nHentar frå smj (Lule Sami)"
+cd ~/git/giellalt/lang-smj && git pull
+echo "\nHentar frå smn (Inari Sami)"
+cd ~/git/giellalt/lang-smn && git pull
+echo "\nHentar frå sms (Skolt Sami)"
+cd ~/git/giellalt/lang-sms && git pull
+echo "\nHentar frå som (Somali)"
+cd ~/git/giellalt/lang-som && git pull
+echo "\nHentar frå sqi (Albanian)"
+cd ~/git/giellalt/lang-sqi && git pull
+echo "\nHentar frå srs (Sarsi)"
+cd ~/git/giellalt/lang-srs && git pull
+echo "\nHentar frå sto (Stoney)"
+cd ~/git/giellalt/lang-sto && git pull
+echo "\nHentar frå swe (Swedish)"
+cd ~/git/giellalt/lang-swe && git pull
+echo "\nHentar frå tat (Tatar)"
+cd ~/git/giellalt/lang-tat && git pull
+echo "\nHentar frå tau (Upper Tanana)"
+cd ~/git/giellalt/lang-tau && git pull
+echo "\nHentar frå tel (Telugu)"
+cd ~/git/giellalt/lang-tel && git pull
+echo "\nHentar frå tgl (Tagalog)"
+cd ~/git/giellalt/lang-tgl && git pull
+echo "\nHentar frå tir (Tigrinya)"
+cd ~/git/giellalt/lang-tir && git pull
+echo "\nHentar frå tku (Upper Necaxa Totonac)"
+cd ~/git/giellalt/lang-tku && git pull
+echo "\nHentar frå tlh (Klingon)"
+cd ~/git/giellalt/lang-tlh && git pull
+echo "\nHentar frå tuv (Turkana)"
+cd ~/git/giellalt/lang-tuv && git pull
+echo "\nHentar frå tyv (Tuvinian)"
+cd ~/git/giellalt/lang-tyv && git pull
+echo "\nHentar frå udm (Udmurt)"
+cd ~/git/giellalt/lang-udm && git pull
+echo "\nHentar frå vep (Veps)"
+cd ~/git/giellalt/lang-vep && git pull
+echo "\nHentar frå vot (Votic)"
+cd ~/git/giellalt/lang-vot && git pull
+echo "\nHentar frå vro (Võro)"
+cd ~/git/giellalt/lang-vro && git pull
+echo "\nHentar frå xal (Kalmyk)"
+cd ~/git/giellalt/lang-xal && git pull
+echo "\nHentar frå xwo (Written Oirat)"
+cd ~/git/giellalt/lang-xwo && git pull
+echo "\nHentar frå yrk (Nenets)"
+cd ~/git/giellalt/lang-yrk && git pull
---
+++ scripts/svn_up_all.sh
@@ -0,0 +1,204 @@
+echo "\n svn up i  aka (Akan)"
+cd ~/git/giellalt/lang-aka && svn up
+echo "\n svn up i  amh (Amharic)"
+cd ~/git/giellalt/lang-amh && svn up
+echo "\n svn up i  apu (Apurinã)"
+cd ~/git/giellalt/lang-apu && svn up
+echo "\n svn up i  aym (Aymara)"
+cd ~/git/giellalt/lang-aym && svn up
+echo "\n svn up i  bak (Bashkir)"
+cd ~/git/giellalt/lang-bak && svn up
+echo "\n svn up i  bla (Siksika)"
+cd ~/git/giellalt/lang-bla && svn up
+echo "\n svn up i  chp (Chipewyan)"
+cd ~/git/giellalt/lang-chp && svn up
+echo "\n svn up i  chr (Cherokee)"
+cd ~/git/giellalt/lang-chr && svn up
+echo "\n svn up i  ciw (Chippewa)"
+cd ~/git/giellalt/lang-ciw && svn up
+echo "\n svn up i  ckt (Chukot)"
+cd ~/git/giellalt/lang-ckt && svn up
+echo "\n svn up i  cor (Cornish)"
+cd ~/git/giellalt/lang-cor && svn up
+echo "\n svn up i  crj (Southern East Cree)"
+cd ~/git/giellalt/lang-crj && svn up
+echo "\n svn up i  crk (Plains Cree)"
+cd ~/git/giellalt/lang-crk && svn up
+echo "\n svn up i  crl (Northern East Cree)"
+cd ~/git/giellalt/lang-crl && svn up
+echo "\n svn up i  deu (German)"
+cd ~/git/giellalt/lang-deu && svn up
+echo "\n svn up i  dgr (Dogrib)"
+cd ~/git/giellalt/lang-dgr && svn up
+echo "\n svn up i  eng (English)"
+cd ~/git/giellalt/lang-eng && svn up
+echo "\n svn up i  epo (Esperanto)"
+cd ~/git/giellalt/lang-epo && svn up
+echo "\n svn up i  ess (Central Siberian Yupik)"
+cd ~/git/giellalt/lang-ess && svn up
+echo "\n svn up i  eus (Basque)"
+cd ~/git/giellalt/lang-eus && svn up
+echo "\n svn up i  evn (Evenki)"
+cd ~/git/giellalt/lang-evn && svn up
+echo "\n svn up i  fao (Faroese)"
+cd ~/git/giellalt/lang-fao && svn up
+echo "\n svn up i  fin (Finnish)"
+cd ~/git/giellalt/lang-fin && svn up
+echo "\n svn up i  fit (Tornedalen Finnish)"
+cd ~/git/giellalt/lang-fit && svn up
+echo "\n svn up i  fkv (Kven Finnish)"
+cd ~/git/giellalt/lang-fkv && svn up
+echo "\n svn up i  gle (Irish)"
+cd ~/git/giellalt/lang-gle && svn up
+echo "\n svn up i  grn (Guarani)"
+cd ~/git/giellalt/lang-grn && svn up
+echo "\n svn up i  hdn (Northern Haida)"
+cd ~/git/giellalt/lang-hdn && svn up
+echo "\n svn up i  hin (Hindi)"
+cd ~/git/giellalt/lang-hin && svn up
+echo "\n svn up i  hun (Hungarian)"
+cd ~/git/giellalt/lang-hun && svn up
+echo "\n svn up i  iku (Inuktitut)"
+cd ~/git/giellalt/lang-iku && svn up
+echo "\n svn up i  ipk (Inupiaq)"
+cd ~/git/giellalt/lang-ipk && svn up
+echo "\n svn up i  izh (Ingrian)"
+cd ~/git/giellalt/lang-izh && svn up
+echo "\n svn up i  kal (Kalaallisut)"
+cd ~/git/giellalt/lang-kal && svn up
+echo "\n svn up i  kca (Khanty)"
+cd ~/git/giellalt/lang-kca && svn up
+echo "\n svn up i  kek (Kekchí)"
+cd ~/git/giellalt/lang-kek && svn up
+echo "\n svn up i  khk (Halh Mongolian)"
+cd ~/git/giellalt/lang-khk && svn up
+echo "\n svn up i  kio (Kiowa)"
+cd ~/git/giellalt/lang-kio && svn up
+echo "\n svn up i  kjh (Khakas)"
+cd ~/git/giellalt/lang-kjh && svn up
+echo "\n svn up i  kmr (Northern Kurdish)"
+cd ~/git/giellalt/lang-kmr && svn up
+echo "\n svn up i  koi (Komi-Permyak)"
+cd ~/git/giellalt/lang-koi && svn up
+echo "\n svn up i  kpv (Komi-Zyrian)"
+cd ~/git/giellalt/lang-kpv && svn up
+echo "\n svn up i  krl (Karelian)"
+cd ~/git/giellalt/lang-krl && svn up
+echo "\n svn up i  lav (Latvian)"
+cd ~/git/giellalt/lang-lav && svn up
+echo "\n svn up i  liv (Liv)"
+cd ~/git/giellalt/lang-liv && svn up
+echo "\n svn up i  luo (Luo in Kenya and Tanzania)"
+cd ~/git/giellalt/lang-luo && svn up
+echo "\n svn up i  lut (Lushootseed)"
+cd ~/git/giellalt/lang-lut && svn up
+echo "\n svn up i  mdf (Moksha)"
+cd ~/git/giellalt/lang-mdf && svn up
+echo "\n svn up i  mhr (Eastern Mari)"
+cd ~/git/giellalt/lang-mhr && svn up
+echo "\n svn up i  mns (Mansi)"
+cd ~/git/giellalt/lang-mns && svn up
+echo "\n svn up i  moe (Montagnais)"
+cd ~/git/giellalt/lang-moe && svn up
+echo "\n svn up i  moh (Mohawk)"
+cd ~/git/giellalt/lang-moh && svn up
+echo "\n svn up i  mrj (Western Mari)"
+cd ~/git/giellalt/lang-mrj && svn up
+echo "\n svn up i  myv (Erzya)"
+cd ~/git/giellalt/lang-myv && svn up
+echo "\n svn up i  ndl (Ndolo)"
+cd ~/git/giellalt/lang-ndl && svn up
+echo "\n svn up i  nds (Low German)"
+cd ~/git/giellalt/lang-nds && svn up
+echo "\n svn up i  nio (Nganasan)"
+cd ~/git/giellalt/lang-nio && svn up
+echo "\n svn up i  nno (Norwegian Nynorsk)"
+cd ~/git/giellalt/lang-nno && svn up
+echo "\n svn up i  nob (Norwegian Bokmål)"
+cd ~/git/giellalt/lang-nob && svn up
+echo "\n svn up i  non (Old Norse)"
+cd ~/git/giellalt/lang-non && svn up
+echo "\n svn up i  nso (Pedi)"
+cd ~/git/giellalt/lang-nso && svn up
+echo "\n svn up i  oji (Ojibwa)"
+cd ~/git/giellalt/lang-oji && svn up
+echo "\n svn up i  olo (Livvi)"
+cd ~/git/giellalt/lang-olo && svn up
+echo "\n svn up i  otw (Ottawa)"
+cd ~/git/giellalt/lang-otw && svn up
+echo "\n svn up i  rmf (Kalo Finnish Romani)"
+cd ~/git/giellalt/lang-rmf && svn up
+echo "\n svn up i  rmn (Balkan Romani)"
+cd ~/git/giellalt/lang-rmn && svn up
+echo "\n svn up i  rmu (Tavringer Romani)"
+cd ~/git/giellalt/lang-rmu && svn up
+echo "\n svn up i  rmy (Vlax Romani)"
+cd ~/git/giellalt/lang-rmy && svn up
+echo "\n svn up i  ron (Romanian)"
+cd ~/git/giellalt/lang-ron && svn up
+echo "\n svn up i  rup (Macedo-Romanian)"
+cd ~/git/giellalt/lang-rup && svn up
+echo "\n svn up i  rus (Russian)"
+cd ~/git/giellalt/lang-rus && svn up
+echo "\n svn up i  sel (Selkup)"
+cd ~/git/giellalt/lang-sel && svn up
+echo "\n svn up i  sjd (Kildin Sami)"
+cd ~/git/giellalt/lang-sjd && svn up
+echo "\n svn up i  sje (Pite Sami)"
+cd ~/git/giellalt/lang-sje && svn up
+echo "\n svn up i  skf (Sakirabiá)"
+cd ~/git/giellalt/lang-skf && svn up
+echo "\n svn up i  sma (Southern Sami)"
+cd ~/git/giellalt/lang-sma && svn up
+echo "\n svn up i  sme (Northern Sami)"
+cd ~/git/giellalt/lang-sme && svn up
+echo "\n svn up i  smj (Lule Sami)"
+cd ~/git/giellalt/lang-smj && svn up
+echo "\n svn up i  smn (Inari Sami)"
+cd ~/git/giellalt/lang-smn && svn up
+echo "\n svn up i  sms (Skolt Sami)"
+cd ~/git/giellalt/lang-sms && svn up
+echo "\n svn up i  som (Somali)"
+cd ~/git/giellalt/lang-som && svn up
+echo "\n svn up i  sqi (Albanian)"
+cd ~/git/giellalt/lang-sqi && svn up
+echo "\n svn up i  srs (Sarsi)"
+cd ~/git/giellalt/lang-srs && svn up
+echo "\n svn up i  sto (Stoney)"
+cd ~/git/giellalt/lang-sto && svn up
+echo "\n svn up i  swe (Swedish)"
+cd ~/git/giellalt/lang-swe && svn up
+echo "\n svn up i  tat (Tatar)"
+cd ~/git/giellalt/lang-tat && svn up
+echo "\n svn up i  tau (Upper Tanana)"
+cd ~/git/giellalt/lang-tau && svn up
+echo "\n svn up i  tel (Telugu)"
+cd ~/git/giellalt/lang-tel && svn up
+echo "\n svn up i  tgl (Tagalog)"
+cd ~/git/giellalt/lang-tgl && svn up
+echo "\n svn up i  tir (Tigrinya)"
+cd ~/git/giellalt/lang-tir && svn up
+echo "\n svn up i  tku (Upper Necaxa Totonac)"
+cd ~/git/giellalt/lang-tku && svn up
+echo "\n svn up i  tlh (Klingon)"
+cd ~/git/giellalt/lang-tlh && svn up
+echo "\n svn up i  tuv (Turkana)"
+cd ~/git/giellalt/lang-tuv && svn up
+echo "\n svn up i  tyv (Tuvinian)"
+cd ~/git/giellalt/lang-tyv && svn up
+echo "\n svn up i  udm (Udmurt)"
+cd ~/git/giellalt/lang-udm && svn up
+echo "\n svn up i  vep (Veps)"
+cd ~/git/giellalt/lang-vep && svn up
+echo "\n svn up i  vot (Votic)"
+cd ~/git/giellalt/lang-vot && svn up
+echo "\n svn up i  vro (Võro)"
+cd ~/git/giellalt/lang-vro && svn up
+echo "\n svn up i  xal (Kalmyk)"
+cd ~/git/giellalt/lang-xal && svn up
+echo "\n svn up i  xwo (Written Oirat)"
+cd ~/git/giellalt/lang-xwo && svn up
+echo "\n svn up i  yrk (Nenets)"
+cd ~/git/giellalt/lang-yrk && svn up
+cd ..
+

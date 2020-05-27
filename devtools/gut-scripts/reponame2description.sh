be40efad3394b0603c8be965970bd772e8482d6e
Sjur N Moshagen <sjurnm@mac.com>
2020-05-27 10:54:48 +0300

    Do not rely on $GTHOME anymore
    Use location of self instead, since it is within giella-core. Also, this script is only used by developers, so we can assume the whole giella-core is available as is, no need for fancy detection of giella-core.

--- devtools/gut-scripts/reponame2description.sh
+++ devtools/gut-scripts/reponame2description.sh
@@ -2,10 +2,14 @@
 
 # test script for gut apply script
 
-# Variables:
+# Input arguments:
 reponame=$1
 orgname=$2
+
+# Other variables:
+selfdir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
 langcode=$(echo $reponame | cut -d'-' -f2)
-langname=$( $GTHOME/giella-core/scripts/iso639-to-name.sh $langcode )
+langname=$( $selfdir/../../scripts/iso639-to-name.sh $langcode )
 
 printf "Finite state and Constraint Grammar based analysers and proofing tools, and language resources for the $langname language"

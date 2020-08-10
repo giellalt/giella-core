fb90af1cc274823da2e048d946bbf899bdd3e1ff
Trond Trosterud <trond.trosterud@uit.no>
2020-08-10 09:34:34 +0200

    Added some more languages. Also added '&&   git config pull.rebase false', although I do not remember adding it. From documentation it seems safer to include it than not.

--- scripts/git_pull_all.sh
+++ scripts/git_pull_all.sh
@@ -1,202 +1,208 @@
 echo "\nHentar frå aka (Akan)"
-cd ~/git/giellalt/lang-aka && git pull
+cd ~/git/giellalt/lang-aka &&   git config pull.rebase false  && git pull
 echo "\nHentar frå amh (Amharic)"
-cd ~/git/giellalt/lang-amh && git pull
+cd ~/git/giellalt/lang-amh &&   git config pull.rebase false  && git pull
 echo "\nHentar frå apu (Apurinã)"
-cd ~/git/giellalt/lang-apu && git pull
+cd ~/git/giellalt/lang-apu &&   git config pull.rebase false  && git pull
 echo "\nHentar frå aym (Aymara)"
-cd ~/git/giellalt/lang-aym && git pull
+cd ~/git/giellalt/lang-aym &&   git config pull.rebase false  && git pull
 echo "\nHentar frå bak (Bashkir)"
-cd ~/git/giellalt/lang-bak && git pull
+cd ~/git/giellalt/lang-bak &&   git config pull.rebase false  && git pull
 echo "\nHentar frå bla (Siksika)"
-cd ~/git/giellalt/lang-bla && git pull
+cd ~/git/giellalt/lang-bla &&   git config pull.rebase false  && git pull
 echo "\nHentar frå chp (Chipewyan)"
-cd ~/git/giellalt/lang-chp && git pull
+cd ~/git/giellalt/lang-chp &&   git config pull.rebase false  && git pull
 echo "\nHentar frå chr (Cherokee)"
-cd ~/git/giellalt/lang-chr && git pull
+cd ~/git/giellalt/lang-chr &&   git config pull.rebase false  && git pull
 echo "\nHentar frå ciw (Chippewa)"
-cd ~/git/giellalt/lang-ciw && git pull
+cd ~/git/giellalt/lang-ciw &&   git config pull.rebase false  && git pull
 echo "\nHentar frå ckt (Chukot)"
-cd ~/git/giellalt/lang-ckt && git pull
+cd ~/git/giellalt/lang-ckt &&   git config pull.rebase false  && git pull
 echo "\nHentar frå cor (Cornish)"
-cd ~/git/giellalt/lang-cor && git pull
+cd ~/git/giellalt/lang-cor &&   git config pull.rebase false  && git pull
 echo "\nHentar frå crj (Southern East Cree)"
-cd ~/git/giellalt/lang-crj && git pull
+cd ~/git/giellalt/lang-crj &&   git config pull.rebase false  && git pull
 echo "\nHentar frå crk (Plains Cree)"
-cd ~/git/giellalt/lang-crk && git pull
-echo "\nHentar frå crl (Northern East Cree)"
-cd ~/git/giellalt/lang-crl && git pull
-echo "\nHentar frå deu (German)"
-cd ~/git/giellalt/lang-deu && git pull
+cd ~/git/giellalt/lang-crk &&   git config pull.rebase false  && git pull
+echo "\nHentar frå cwd (Woods Cree)"
+cd ~/git/giellalt/lang-cwd &&   git config pull.rebase false  && git pull
 echo "\nHentar frå dgr (Dogrib)"
-cd ~/git/giellalt/lang-dgr && git pull
+cd ~/git/giellalt/lang-dgr &&   git config pull.rebase false  && git pull
 echo "\nHentar frå eng (English)"
-cd ~/git/giellalt/lang-eng && git pull
+cd ~/git/giellalt/lang-eng &&   git config pull.rebase false  && git pull
 echo "\nHentar frå epo (Esperanto)"
-cd ~/git/giellalt/lang-epo && git pull
+cd ~/git/giellalt/lang-epo &&   git config pull.rebase false  && git pull
 echo "\nHentar frå ess (Central Siberian Yupik)"
-cd ~/git/giellalt/lang-ess && git pull
+cd ~/git/giellalt/lang-ess &&   git config pull.rebase false  && git pull
 echo "\nHentar frå eus (Basque)"
-cd ~/git/giellalt/lang-eus && git pull
+cd ~/git/giellalt/lang-eus &&   git config pull.rebase false  && git pull
 echo "\nHentar frå evn (Evenki)"
-cd ~/git/giellalt/lang-evn && git pull
+cd ~/git/giellalt/lang-evn &&   git config pull.rebase false  && git pull
 echo "\nHentar frå fao (Faroese)"
-cd ~/git/giellalt/lang-fao && git pull
+cd ~/git/giellalt/lang-fao &&   git config pull.rebase false  && git pull
 echo "\nHentar frå fin (Finnish)"
-cd ~/git/giellalt/lang-fin && git pull
+cd ~/git/giellalt/lang-fin &&   git config pull.rebase false  && git pull
 echo "\nHentar frå fit (Tornedalen Finnish)"
-cd ~/git/giellalt/lang-fit && git pull
+cd ~/git/giellalt/lang-fit &&   git config pull.rebase false  && git pull
 echo "\nHentar frå fkv (Kven Finnish)"
-cd ~/git/giellalt/lang-fkv && git pull
+cd ~/git/giellalt/lang-fkv &&   git config pull.rebase false  && git pull
 echo "\nHentar frå gle (Irish)"
-cd ~/git/giellalt/lang-gle && git pull
+cd ~/git/giellalt/lang-gle &&   git config pull.rebase false  && git pull
 echo "\nHentar frå grn (Guarani)"
-cd ~/git/giellalt/lang-grn && git pull
+cd ~/git/giellalt/lang-grn &&   git config pull.rebase false  && git pull
 echo "\nHentar frå hdn (Northern Haida)"
-cd ~/git/giellalt/lang-hdn && git pull
+cd ~/git/giellalt/lang-hdn &&   git config pull.rebase false  && git pull
 echo "\nHentar frå hin (Hindi)"
-cd ~/git/giellalt/lang-hin && git pull
+cd ~/git/giellalt/lang-hin &&   git config pull.rebase false  && git pull
 echo "\nHentar frå hun (Hungarian)"
-cd ~/git/giellalt/lang-hun && git pull
+cd ~/git/giellalt/lang-hun &&   git config pull.rebase false  && git pull
 echo "\nHentar frå iku (Inuktitut)"
-cd ~/git/giellalt/lang-iku && git pull
+cd ~/git/giellalt/lang-iku &&   git config pull.rebase false  && git pull
 echo "\nHentar frå ipk (Inupiaq)"
-cd ~/git/giellalt/lang-ipk && git pull
+cd ~/git/giellalt/lang-ipk &&   git config pull.rebase false  && git pull
 echo "\nHentar frå izh (Ingrian)"
-cd ~/git/giellalt/lang-izh && git pull
+cd ~/git/giellalt/lang-izh &&   git config pull.rebase false  && git pull
 echo "\nHentar frå kal (Kalaallisut)"
-cd ~/git/giellalt/lang-kal && git pull
+cd ~/git/giellalt/lang-kal &&   git config pull.rebase false  && git pull
 echo "\nHentar frå kca (Khanty)"
-cd ~/git/giellalt/lang-kca && git pull
+cd ~/git/giellalt/lang-kca &&   git config pull.rebase false  && git pull
 echo "\nHentar frå kek (Kekchí)"
-cd ~/git/giellalt/lang-kek && git pull
+cd ~/git/giellalt/lang-kek &&   git config pull.rebase false  && git pull
 echo "\nHentar frå khk (Halh Mongolian)"
-cd ~/git/giellalt/lang-khk && git pull
+cd ~/git/giellalt/lang-khk &&   git config pull.rebase false  && git pull
 echo "\nHentar frå kio (Kiowa)"
-cd ~/git/giellalt/lang-kio && git pull
+cd ~/git/giellalt/lang-kio &&   git config pull.rebase false  && git pull
 echo "\nHentar frå kjh (Khakas)"
-cd ~/git/giellalt/lang-kjh && git pull
+cd ~/git/giellalt/lang-kjh &&   git config pull.rebase false  && git pull
 echo "\nHentar frå kmr (Northern Kurdish)"
-cd ~/git/giellalt/lang-kmr && git pull
+cd ~/git/giellalt/lang-kmr &&   git config pull.rebase false  && git pull
 echo "\nHentar frå koi (Komi-Permyak)"
-cd ~/git/giellalt/lang-koi && git pull
+cd ~/git/giellalt/lang-koi &&   git config pull.rebase false  && git pull
 echo "\nHentar frå kpv (Komi-Zyrian)"
-cd ~/git/giellalt/lang-kpv && git pull
+cd ~/git/giellalt/lang-kpv &&   git config pull.rebase false  && git pull
 echo "\nHentar frå krl (Karelian)"
-cd ~/git/giellalt/lang-krl && git pull
+cd ~/git/giellalt/lang-krl &&   git config pull.rebase false  && git pull
 echo "\nHentar frå lav (Latvian)"
-cd ~/git/giellalt/lang-lav && git pull
+cd ~/git/giellalt/lang-lav &&   git config pull.rebase false  && git pull
 echo "\nHentar frå liv (Liv)"
-cd ~/git/giellalt/lang-liv && git pull
+cd ~/git/giellalt/lang-liv &&   git config pull.rebase false  && git pull
 echo "\nHentar frå luo (Luo in Kenya and Tanzania)"
-cd ~/git/giellalt/lang-luo && git pull
+cd ~/git/giellalt/lang-luo &&   git config pull.rebase false  && git pull
 echo "\nHentar frå lut (Lushootseed)"
-cd ~/git/giellalt/lang-lut && git pull
+cd ~/git/giellalt/lang-lut &&   git config pull.rebase false  && git pull
 echo "\nHentar frå mdf (Moksha)"
-cd ~/git/giellalt/lang-mdf && git pull
+cd ~/git/giellalt/lang-mdf &&   git config pull.rebase false  && git pull
 echo "\nHentar frå mhr (Eastern Mari)"
-cd ~/git/giellalt/lang-mhr && git pull
+cd ~/git/giellalt/lang-mhr &&   git config pull.rebase false  && git pull
 echo "\nHentar frå mns (Mansi)"
-cd ~/git/giellalt/lang-mns && git pull
-echo "\nHentar frå moe (Montagnais)"
-cd ~/git/giellalt/lang-moe && git pull
+cd ~/git/giellalt/lang-mns &&   git config pull.rebase false  && git pull
 echo "\nHentar frå moh (Mohawk)"
-cd ~/git/giellalt/lang-moh && git pull
+cd ~/git/giellalt/lang-moh &&   git config pull.rebase false  && git pull
 echo "\nHentar frå mrj (Western Mari)"
-cd ~/git/giellalt/lang-mrj && git pull
+cd ~/git/giellalt/lang-mrj &&   git config pull.rebase false  && git pull
 echo "\nHentar frå myv (Erzya)"
-cd ~/git/giellalt/lang-myv && git pull
+cd ~/git/giellalt/lang-myv &&   git config pull.rebase false  && git pull
 echo "\nHentar frå ndl (Ndolo)"
-cd ~/git/giellalt/lang-ndl && git pull
+cd ~/git/giellalt/lang-ndl &&   git config pull.rebase false  && git pull
 echo "\nHentar frå nds (Low German)"
-cd ~/git/giellalt/lang-nds && git pull
+cd ~/git/giellalt/lang-nds &&   git config pull.rebase false  && git pull
 echo "\nHentar frå nio (Nganasan)"
-cd ~/git/giellalt/lang-nio && git pull
+cd ~/git/giellalt/lang-nio &&   git config pull.rebase false  && git pull
 echo "\nHentar frå nno (Norwegian Nynorsk)"
-cd ~/git/giellalt/lang-nno && git pull
+cd ~/git/giellalt/lang-nno &&   git config pull.rebase false  && git pull
 echo "\nHentar frå nob (Norwegian Bokmål)"
-cd ~/git/giellalt/lang-nob && git pull
+cd ~/git/giellalt/lang-nob &&   git config pull.rebase false  && git pull
 echo "\nHentar frå non (Old Norse)"
-cd ~/git/giellalt/lang-non && git pull
+cd ~/git/giellalt/lang-non &&   git config pull.rebase false  && git pull
 echo "\nHentar frå nso (Pedi)"
-cd ~/git/giellalt/lang-nso && git pull
+cd ~/git/giellalt/lang-nso &&   git config pull.rebase false  && git pull
 echo "\nHentar frå oji (Ojibwa)"
-cd ~/git/giellalt/lang-oji && git pull
+cd ~/git/giellalt/lang-oji &&   git config pull.rebase false  && git pull
 echo "\nHentar frå olo (Livvi)"
-cd ~/git/giellalt/lang-olo && git pull
-echo "\nHentar frå otw (Ottawa)"
-cd ~/git/giellalt/lang-otw && git pull
+cd ~/git/giellalt/lang-olo &&   git config pull.rebase false  && git pull
 echo "\nHentar frå rmf (Kalo Finnish Romani)"
-cd ~/git/giellalt/lang-rmf && git pull
+cd ~/git/giellalt/lang-rmf &&   git config pull.rebase false  && git pull
 echo "\nHentar frå rmn (Balkan Romani)"
-cd ~/git/giellalt/lang-rmn && git pull
+cd ~/git/giellalt/lang-rmn &&   git config pull.rebase false  && git pull
 echo "\nHentar frå rmu (Tavringer Romani)"
-cd ~/git/giellalt/lang-rmu && git pull
+cd ~/git/giellalt/lang-rmu &&   git config pull.rebase false  && git pull
 echo "\nHentar frå rmy (Vlax Romani)"
-cd ~/git/giellalt/lang-rmy && git pull
+cd ~/git/giellalt/lang-rmy &&   git config pull.rebase false  && git pull
 echo "\nHentar frå ron (Romanian)"
-cd ~/git/giellalt/lang-ron && git pull
+cd ~/git/giellalt/lang-ron &&   git config pull.rebase false  && git pull
 echo "\nHentar frå rup (Macedo-Romanian)"
-cd ~/git/giellalt/lang-rup && git pull
+cd ~/git/giellalt/lang-rup &&   git config pull.rebase false  && git pull
 echo "\nHentar frå rus (Russian)"
-cd ~/git/giellalt/lang-rus && git pull
+cd ~/git/giellalt/lang-rus &&   git config pull.rebase false  && git pull
 echo "\nHentar frå sel (Selkup)"
-cd ~/git/giellalt/lang-sel && git pull
+cd ~/git/giellalt/lang-sel &&   git config pull.rebase false  && git pull
 echo "\nHentar frå sjd (Kildin Sami)"
-cd ~/git/giellalt/lang-sjd && git pull
+cd ~/git/giellalt/lang-sjd &&   git config pull.rebase false  && git pull
+echo "\nHentar frå Kildin (Ter Sami)"
+cd ~/git/giellalt/lang-sjt &&   git config pull.rebase false  && git pull
 echo "\nHentar frå sje (Pite Sami)"
-cd ~/git/giellalt/lang-sje && git pull
+cd ~/git/giellalt/lang-sje &&   git config pull.rebase false  && git pull
 echo "\nHentar frå skf (Sakirabiá)"
-cd ~/git/giellalt/lang-skf && git pull
+cd ~/git/giellalt/lang-skf &&   git config pull.rebase false  && git pull
 echo "\nHentar frå sma (Southern Sami)"
-cd ~/git/giellalt/lang-sma && git pull
+cd ~/git/giellalt/lang-sma &&   git config pull.rebase false  && git pull
 echo "\nHentar frå sme (Northern Sami)"
-cd ~/git/giellalt/lang-sme && git pull
+cd ~/git/giellalt/lang-sme &&   git config pull.rebase false  && git pull
 echo "\nHentar frå smj (Lule Sami)"
-cd ~/git/giellalt/lang-smj && git pull
+cd ~/git/giellalt/lang-smj &&   git config pull.rebase false  && git pull
 echo "\nHentar frå smn (Inari Sami)"
-cd ~/git/giellalt/lang-smn && git pull
+cd ~/git/giellalt/lang-smn &&   git config pull.rebase false  && git pull
 echo "\nHentar frå sms (Skolt Sami)"
-cd ~/git/giellalt/lang-sms && git pull
+cd ~/git/giellalt/lang-sms &&   git config pull.rebase false  && git pull
 echo "\nHentar frå som (Somali)"
-cd ~/git/giellalt/lang-som && git pull
+cd ~/git/giellalt/lang-som &&   git config pull.rebase false  && git pull
 echo "\nHentar frå sqi (Albanian)"
-cd ~/git/giellalt/lang-sqi && git pull
+cd ~/git/giellalt/lang-sqi &&   git config pull.rebase false  && git pull
 echo "\nHentar frå srs (Sarsi)"
-cd ~/git/giellalt/lang-srs && git pull
+cd ~/git/giellalt/lang-srs &&   git config pull.rebase false  && git pull
 echo "\nHentar frå sto (Stoney)"
-cd ~/git/giellalt/lang-sto && git pull
+cd ~/git/giellalt/lang-sto &&   git config pull.rebase false  && git pull
 echo "\nHentar frå swe (Swedish)"
-cd ~/git/giellalt/lang-swe && git pull
+cd ~/git/giellalt/lang-swe &&   git config pull.rebase false  && git pull
 echo "\nHentar frå tat (Tatar)"
-cd ~/git/giellalt/lang-tat && git pull
+cd ~/git/giellalt/lang-tat &&   git config pull.rebase false  && git pull
 echo "\nHentar frå tau (Upper Tanana)"
-cd ~/git/giellalt/lang-tau && git pull
+cd ~/git/giellalt/lang-tau &&   git config pull.rebase false  && git pull
 echo "\nHentar frå tel (Telugu)"
-cd ~/git/giellalt/lang-tel && git pull
+cd ~/git/giellalt/lang-tel &&   git config pull.rebase false  && git pull
 echo "\nHentar frå tgl (Tagalog)"
-cd ~/git/giellalt/lang-tgl && git pull
+cd ~/git/giellalt/lang-tgl &&   git config pull.rebase false  && git pull
 echo "\nHentar frå tir (Tigrinya)"
-cd ~/git/giellalt/lang-tir && git pull
+cd ~/git/giellalt/lang-tir &&   git config pull.rebase false  && git pull
 echo "\nHentar frå tku (Upper Necaxa Totonac)"
-cd ~/git/giellalt/lang-tku && git pull
+cd ~/git/giellalt/lang-tku &&   git config pull.rebase false  && git pull
 echo "\nHentar frå tlh (Klingon)"
-cd ~/git/giellalt/lang-tlh && git pull
+cd ~/git/giellalt/lang-tlh &&   git config pull.rebase false  && git pull
 echo "\nHentar frå tuv (Turkana)"
-cd ~/git/giellalt/lang-tuv && git pull
+cd ~/git/giellalt/lang-tuv &&   git config pull.rebase false  && git pull
 echo "\nHentar frå tyv (Tuvinian)"
-cd ~/git/giellalt/lang-tyv && git pull
+cd ~/git/giellalt/lang-tyv &&   git config pull.rebase false  && git pull
 echo "\nHentar frå udm (Udmurt)"
-cd ~/git/giellalt/lang-udm && git pull
+cd ~/git/giellalt/lang-udm &&   git config pull.rebase false  && git pull
 echo "\nHentar frå vep (Veps)"
-cd ~/git/giellalt/lang-vep && git pull
+cd ~/git/giellalt/lang-vep &&   git config pull.rebase false  && git pull
 echo "\nHentar frå vot (Votic)"
-cd ~/git/giellalt/lang-vot && git pull
+cd ~/git/giellalt/lang-vot &&   git config pull.rebase false  && git pull
 echo "\nHentar frå vro (Võro)"
-cd ~/git/giellalt/lang-vro && git pull
+cd ~/git/giellalt/lang-vro &&   git config pull.rebase false  && git pull
 echo "\nHentar frå xal (Kalmyk)"
-cd ~/git/giellalt/lang-xal && git pull
+cd ~/git/giellalt/lang-xal &&   git config pull.rebase false  && git pull
 echo "\nHentar frå xwo (Written Oirat)"
-cd ~/git/giellalt/lang-xwo && git pull
+cd ~/git/giellalt/lang-xwo &&   git config pull.rebase false  && git pull
 echo "\nHentar frå yrk (Nenets)"
-cd ~/git/giellalt/lang-yrk && git pull
+cd ~/git/giellalt/lang-yrk &&   git config pull.rebase false  && git pull
+#echo "\nHentar frå otw (Ottawa)"
+#cd ~/git/giellalt/lang-otw &&   git config pull.rebase false  && git pull
+#echo "\nHentar frå crl (Northern East Cree)"
+#cd ~/git/giellalt/lang-crl &&   git config pull.rebase false  && git pull
+#echo "\nHentar frå moe (Montagnais)"
+#cd ~/git/giellalt/lang-moe &&   git config pull.rebase false  && git pull
+echo "\nHentar frå giella-core"
+cd ~/git/giellalt/giella-core &&   git config pull.rebase false  && git pull
+echo "\nHentar frå giella-shared"
+cd ~/git/giellalt/giella-shared &&   git config pull.rebase false  && git pull

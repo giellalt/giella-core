e8ecc2b08b07f82f0d4bb428a8339e5aa1110afb
Sjur N Moshagen <sjurnm@mac.com>
2020-08-19 13:46:11 +0300

    Add script to run autogen & configure from gut
    ie in all repos matching regex

---
+++ devtools/gut-scripts/autogen_and_configure.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# gut apply script to run autogen.sh and configure with defaults, to update
+# generated and committed files like manifest.toml:
+
+# command:
+./autogen.sh && ./configure

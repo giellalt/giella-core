577470e0f346cb4df94e882a3739143bec749cd1
Sjur N Moshagen <sjurnm@mac.com>
2020-09-17 16:25:15 +0300

    Make test zsh friendly (POSIX compatible)

--- devtools/init.d/lookup-init.sh
+++ devtools/init.d/lookup-init.sh
@@ -4,7 +4,7 @@
 
 HOSTNAME=`hostname`
 
-if [ $HOSTNAME == 'victorio.uit.no' ]
+if [ $HOSTNAME = 'victorio.uit.no' ]
 then export LOOKUP='/opt/sami/xerox/c-fsm/ix86-linux2.6-gcc3.4/bin/lookup -flags  mbTT -utf8'
 #then export LOOKUP='/opt/sami/xerox/c-fsm/ix86-linux2.6-gcc3.4/bin/lookup -q -flags  mbTT -utf8'
 #else export LOOKUP='lookup -flags mbTT' # we try with quiet mode

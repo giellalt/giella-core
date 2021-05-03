be02b8aaa28283861335e59816f9e66ba69b32e7
Sjur N Moshagen <sjurnm@mac.com>
2021-05-03 12:00:55 +0300

    Now that the branch has been removed from GH, only remove locally

--- devtools/gut-scripts/remove-branch.sh
+++ devtools/gut-scripts/remove-branch.sh
@@ -3,5 +3,4 @@
 # gut apply script to revert last commit:
 
 # git command:
-# git fetch --all && git checkout main && git branch -D develop && git push origin --delete develop
-git push origin --delete develop
+git fetch --all && git checkout main && git branch -D develop

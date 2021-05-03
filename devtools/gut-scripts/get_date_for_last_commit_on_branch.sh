33ab97bd69c2d9b2f6da8010e1947dbbca7a58d5
Sjur N Moshagen <sjurnm@mac.com>
2021-05-03 14:39:04 +0300

    Include your personal access token to avoid being hit by random rate limits
    Replace OAUTH-TOKEN with your real token.

--- devtools/gut-scripts/get_date_for_last_commit_on_branch.sh
+++ devtools/gut-scripts/get_date_for_last_commit_on_branch.sh
@@ -4,4 +4,5 @@
 
 reponame=$(basename $(pwd))
 
-curl -s -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/giellalt/$reponame/commits/master | grep "date" | head -n +1 | cut -d':' -f2 | cut -d'"' -f2 | cut -d'T' -f1
+# Replace OAUTH-TOKEN with your real token.
+curl -s -H "Accept: application/vnd.github.v3+json" curl -H "Authorization: token OAUTH-TOKEN" https://api.github.com/repos/giellalt/$reponame/commits/master | grep "date" | head -n +1 | cut -d':' -f2 | cut -d'"' -f2 | cut -d'T' -f1

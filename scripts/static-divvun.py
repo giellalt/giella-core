e15ddb0e83f962f8fe571429b0a252fd1246406b
Børre Gaup <albbas@gmail.com>
2021-11-29 14:48:49 +0100

    A nice logging format

--- scripts/static-divvun.py
+++ scripts/static-divvun.py
@@ -19,7 +19,11 @@ import subprocess
 
 import lxml.etree as etree
 
-logging.basicConfig(level=logging.INFO)
+logging.basicConfig(
+    format="%(asctime)s,%(msecs)d %(levelname)-8s [%(filename)s:%(lineno)d] %(message)s",
+    datefmt="%Y-%m-%d:%H:%M:%S",
+    level=logging.INFO,
+)
 logger = logging.getLogger(__name__)
 
 

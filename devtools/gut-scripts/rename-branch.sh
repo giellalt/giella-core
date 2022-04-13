8ab8e660b8a50f43d6dc857ad0e8eeedd1b5e028
Sjur N Moshagen <sjur.n.moshagen@uit.no>
2022-04-13 18:27:53 +0300

    Add another utility script

---
+++ devtools/gut-scripts/rename-branch.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# Script to rename branch for all matching repos
+# Cf https://www.theserverside.com/blog/Coffee-Talk-Java-News-Stories-and-Opinions/rename-Git-branch-local-remote-GitHub-GitLab-BitBucket
+
+git branch -m main

8ddf6195e79c97cc7e646b2855eff23f5ca3a85d
Sjur N Moshagen <sjurnm@mac.com>
2022-11-16 08:54:41 +0200

    Add script to convert layouts from kbdgen 2 to 3 format

---
+++ devtools/gut-scripts/convert_kbdgen_2to3.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Script for gut apply --script that converts keyboard layouts from kbdgen2 to
+# kbdgen3 format.
+
+# Variables:
+convertordir=../../divvun/kbdgen/resources/scripts
+
+for layout in *.kbdgen/layouts/*.yaml; do
+    echo $layout
+    # deno run --allow-read $convertordir/kbdgen2to3.js $layout
+done

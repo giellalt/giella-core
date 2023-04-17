96fdd76d9ca3910aabc158d254a0bb4d6aa6fe62
Flammie A Pirinen <flammie@iki.fi>
2023-04-17 18:02:07 +0200

    coverage script for article counts

---
+++ scripts/coverage-etc.bash
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+if test $# -lt 1 ; then
+    echo "Usage: $0 LANGUAGECODEs..."
+    echo
+    exit 1
+fi
+
+for ll in "$@" ; do
+    echo -n words in $ll: 
+    cat "$GTLANGS/lang-$ll"/src/fst/stems/*.lexc |\
+        sed -e 's/!.*//' |\
+        grep -F -v LEXICON |\
+        grep -E -v '^[[:space:]]*$' |\
+        wc -l
+    echo -n covarega for $ll: 
+    ccat "$GTFREE/converted/$ll" |\
+        sed -e 's/¶//g' -e 's:[]^/{}<>@$[]:\\&:g' |\
+        hfst-proc "$GTLANGS/lang-$ll"/src/analyser-gt-desc.hfstol
+
+done

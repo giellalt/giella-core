fa3e5d8f4f9b125ed0f17ac2a25ea172b02e8bb1
Sjur N Moshagen <sjurnm@mac.com>
2023-04-18 12:37:30 +0300

    Escape homonym tags

--- devtools/gut-scripts/get_lemma_count.sh
+++ devtools/gut-scripts/get_lemma_count.sh
@@ -8,6 +8,10 @@
 # -r '^lang-(mhr|myv|fao |fin|kal|izh|sjd|kpv|fkv|olo|mdf|sje|sm.|udm|vro|mrj)$'
 
 lemmacount=$(grep ';' src/fst/stems/*.lexc | # get all entries in the stems dir
+             sed 's/\+Hom/_Hom/'       | # protect homonym tags
+             sed 's/\+G3/_G3/'         | # protect SME homonym tag G3
+             sed 's/\+G7/_G7/'         | # protect SME homonym tag G7
+             sed 's/\+NomAg/_NomAg/'   | # protect SME homonym tag NomAg
              cut -d':' -f2-            | # get rid of the filename part of the grep matches
              egrep -v '^\s*(:|\+|!|@)' | # remove lines starting :+!@
              egrep -v '^\s*[^\s:]+\s+;'| # remove lines with only contlex

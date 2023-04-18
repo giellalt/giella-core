64202d8c0f6a80c3d2abf1aaea29379237daffff
Flammie A Pirinen <flammie@iki.fi>
2023-04-18 09:28:30 +0200

    whole calc

--- scripts/coverage-etc.bash
+++ scripts/coverage-etc.bash
@@ -7,15 +7,19 @@ if test $# -lt 1 ; then
 fi
 
 for ll in "$@" ; do
-    echo -n words in $ll: 
+    echo -n "words in $ll: "
     cat "$GTLANGS/lang-$ll"/src/fst/stems/*.lexc |\
         sed -e 's/!.*//' |\
         grep -F -v LEXICON |\
         grep -E -v '^[[:space:]]*$' |\
         wc -l
-    echo -n covarega for $ll: 
+    echo -n "covarega for $ll: "
     ccat "$GTFREE/converted/$ll" |\
-        sed -e 's/¶//g' -e 's:[]^/{}<>@$[]:\\&:g' |\
-        hfst-proc "$GTLANGS/lang-$ll"/src/analyser-gt-desc.hfstol
-
+        sed -e 's/¶//g' -e 's:[]^/{}\\@$[]:\\&:g' -e 's/[<>]//g' |\
+        hfst-proc "$GTLANGS/lang-$ll"/src/analyser-gt-desc.hfstol > "$ll.apes"
+    tr '^' '\n' < "$ll.apes" > "$ll.tokens"
+    echo -n "tokens: "
+    wc -l < "$ll.tokens"
+    echo -n "OOVs: "
+    fgrep -F -c "/*" < "$ll.tokens"
 done

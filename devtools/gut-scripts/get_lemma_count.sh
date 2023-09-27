16de45676b2c3f16230f68b3e9638f7730cf57e7
Sjur N Moshagen <sjurnm@mac.com>
2023-09-27 10:28:14 +0300

    Replace gross lemma extraction with the canonical one, giving much more exact results

--- devtools/gut-scripts/get_lemma_count.sh
+++ devtools/gut-scripts/get_lemma_count.sh
@@ -7,17 +7,12 @@
 # gut apply -s giella-core/devtools/gut-scripts/get_lemma_count.sh \
 # -r '^lang-(mhr|myv|fao |fin|kal|izh|sjd|kpv|fkv|olo|mdf|sje|sm.|udm|vro|mrj)$'
 
-lemmacount=$(grep ';' src/fst/stems/*.lexc | # get all entries in the stems dir
-             sed 's/\+Hom/_Hom/'       | # protect homonym tags
-             sed 's/\+G3/_G3/'         | # protect SME homonym tag G3
-             sed 's/\+G7/_G7/'         | # protect SME homonym tag G7
-             sed 's/\+NomAg/_NomAg/'   | # protect SME homonym tag NomAg
-             cut -d':' -f2-            | # get rid of the filename part of the grep matches
-             egrep -v '^\s*(:|\+|!|@)' | # remove lines starting :+!@
-             egrep -v '^\s*[^\s:]+\s+;'| # remove lines with only contlex
-             cut -d':' -f1             | # remove surface side
-             cut -d'+' -f1             | # remove tags
-             sort -u                   | # sort and unique - we only count unique lemmas 
-             wc -l)
+# Use common lemma extraction script for most robust and reliable count:
+lemmacount=$(for f in src/fst/stems/*.lexc ; do 
+    echo $(../giella-core/scripts/extract-lemmas.sh -H $f | # extract all lemmas for each stem file, keeping homonyms
+    wc -l); done  | # ... and count them
+    tr '\n' '+'   | # replace newline with +
+    sed 's/\+$//' | # ... remove the final +, and summarise everything in the final command
+    bc)
 
 echo $lemmacount

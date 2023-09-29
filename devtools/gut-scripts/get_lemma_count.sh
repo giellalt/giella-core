0fd72135c0783c2b009d701fa85a8feb3eaceea6
Sjur N Moshagen <sjurnm@mac.com>
2023-09-29 11:44:14 +0300

    Use the new lemma counting script

--- devtools/gut-scripts/get_lemma_count.sh
+++ devtools/gut-scripts/get_lemma_count.sh
@@ -7,12 +7,9 @@
 # gut apply -s giella-core/devtools/gut-scripts/get_lemma_count.sh \
 # -r '^lang-(mhr|myv|fao|fin|kal|izh|sjd|kpv|fkv|olo|mdf|sje|sm.|udm|vro|mrj)$'
 
+dir=$(pwd)
+
 # Use common lemma extraction script for most robust and reliable count:
-lemmacount=$(for f in src/fst/stems/*.lexc ; do 
-    echo $(../giella-core/scripts/extract-lemmas.sh -H $f | # extract all lemmas for each stem file, keeping homonyms
-    wc -l); done  | # ... and count them
-    tr '\n' '+'   | # replace newline with +
-    sed 's/\+$//' | # ... remove the final +, and summarise everything in the final command
-    bc)
+lemmacount=$(../giella-core/scripts/count-all-lemmas.sh $dir )
 
 echo $lemmacount

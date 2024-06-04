d7dabb97d4bb33c1f64419180614adba29cc01e9
Børre Gaup <albbas@gmail.com>
2024-06-04 16:35:47 +0200

    Edit pre-commit installation suggestions

--- autogen.sh
+++ autogen.sh
@@ -8,6 +8,6 @@ autoreconf -i -v
 
 if ! type pre-commit > /dev/null 2>&1 ; then
     echo "we recommend use of pre-commit for automatic checks and fixes:"
-    echo "  on mac: sudo brew install pre-commit"
-    echo "  on many others: sudo python3 -m pip install pre-commit"
+    echo "  on mac: brew install pre-commit"
+    echo "  on others: pipx install pre-commit"
 fi

7a80cb01daed9d9dcf95db0aeb6258cf5f7cd9f2
Flammie A Pirinen <flammie@iki.fi>
2024-06-12 03:07:14 +0200

    helper scripts

---
+++ devtools/make-checker.bash
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+if ! test -d lang-zxx ; then
+    echo "run this script in giellalt gut root that has lang-* subdirs!"
+    exit 2
+fi
+for f in lang-* ; do
+    echo "skip $f?"
+    select answer in yes no ; do
+        if test $answer == yes ; then
+            continue 2
+        else
+            break
+        fi
+    done
+    pushd "$f" || exit 1
+    ./autogen.sh || exit 1
+    ./configure "$@" || exit 1
+    if ! make ; then
+        echo this needs to be fixed later...
+        echo "$f" >> broken-makes
+        popd || exit 1
+        continue
+    fi
+    make check
+    echo repeat?
+    select answer in yes no ; do
+        if test $answer == yes ; then
+            make check
+        else
+            break
+        fi
+    done
+    popd || exit 1
+done

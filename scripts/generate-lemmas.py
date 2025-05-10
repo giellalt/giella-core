628c3e59a4537a80b93ee389fa26ff13f0e71479
Flammie A Pirinen <flammie@iki.fi>
2025-05-10 17:25:10 +0200

    generate lemmas in python

--- scripts/generate-lemmas.py
+++ scripts/generate-lemmas.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import re
 import sys
 import tempfile
 from argparse import ArgumentParser
@@ -7,7 +8,7 @@ from argparse import ArgumentParser
 try:
     import hfst
 except ImportError:
-    print("skip - missing python hfst libary")
+    print("skip - Missing python hfst libary")
     sys.exit(77)
 
 
@@ -24,6 +25,13 @@ def hidelexcescapes(s: str) -> str:
     s = s.replace("%:", "@COLON@")
     s = s.replace("%<", "@LESSTHAN@")
     s = s.replace("% ", "@SPACE@")
+    if "<" in s and ">" in s:
+        s = re.sub("<.*>", "@REGEX@", s)
+    if "\"" in s:
+        s = s.replace("%\"", "@QUOTATIONMARK@")
+        if "\"" in s:
+            # archaic translation comment
+            s = re.sub("\".*\"", "", s)
     return s
 
 
@@ -33,24 +41,35 @@ def unhidelexcescapes(s: str, unescape=True) -> str:
         s = s.replace("@COLON@", ":")
         s = s.replace("@LESSTHAN@", "<")
         s = s.replace("@SPACE@", " ")
+        s = s.replace("@QUOTATIONMARK@", "\"")
     else:
         s = s.replace("@EXCLAMATIONMARK@", "%!")
         s = s.replace("@COLON@", ":")
         s = s.replace("@LESSTHAN@", "<")
         s = s.replace("@SPACE@", " ")
+        s = s.replace("@QUOTATIONMARK@", "%\"")
+    return s
+
+
+def killflagdiacritics(s: str) -> str:
+    if "@" in s:
+        s = re.sub("@[CRDPN].[^@]*@", "", s)
     return s
 
 
 def main():
     argp = ArgumentParser()
-    argp.add_argument("-p", "--paradigm", type=open, dest="paradigmfile",
-                      help="list of tagstrings making up the paradigm",
-                      required=True)
     argp.add_argument("-l", "--lexc", type=open, dest="lexcfile",
                       help="read lemmas from the lexc file",
                       required=True)
+    argp.add_argument("-a", "--analyser", type=str, dest="analyserfilename",
+                      help="FST analyser for analysing missing lemmas",
+                      required=True)
     argp.add_argument("-g", "--generator", type=str, dest="generatorfilename",
                       help="FST generator file for generating the forms",
+                      required=True)
+    argp.add_argument("-t", "--tags", action="append",
+                      help="tags that lemma form should have in generator",
                       required=True)
     argp.add_argument("-T", "--threshold", type=int,
                       help="required % proportion of succesful generations",
@@ -59,30 +78,38 @@ def main():
                       help="prints debugging outputs")
     argp.add_argument("-v", "--verbose", action="store_true", default=False,
                       help="prints some outputs")
-    argp.add_argument("-X", "--acceptable-tags", action="append",
-                      help="do not count oov if analyses contain these tags")
     argp.add_argument("-Z", "--acceptable-forms", type=open,
                       help="do not count oov if analysis contained in file")
+    argp.add_argument("-X", "--exclude", action="append",
+                      help="exclude lines matching regex")
+    argp.add_argument("-Q", "--oov-limit", type=int, default=1000,
+                      help="stop trying after so many oovs")
     options = argp.parse_args()
     logfile = tempfile.NamedTemporaryFile(prefix="paradigm", suffix=".txt",
                                           delete=False, encoding="UTF-8",
                                           mode="w+")
     generator = load_hfst(options.generatorfilename)
-    paradigms = [l.strip() for l in options.paradigmfile.readlines() if
-                 l.strip() != ""]
+    analyser = load_hfst(options.analyserfilename)
     skipforms = None
     if options.acceptable_forms:
         skipforms = [l.strip() for l in options.acceptable_forms.readlines()]
-    skiptags = options.acceptable_tags
     lemmas = set()
     for lexcline in options.lexcfile:
         if not lexcline or lexcline.strip() == "":
             continue
+        if options.exclude:
+            excluded = False
+            for exclusion in options.exclude:
+                if re.search(exclusion, lexcline):
+                    excluded = True
+            if excluded:
+                continue
         # preproc
         lexcline = hidelexcescapes(lexcline)
         if "!" in lexcline:
             lexcline = lexcline.split("!")[0]
         lexcline = lexcline.strip()
+        lexcline = killflagdiacritics(lexcline)
         # see stuff
         if lexcline.startswith("LEXICON "):
             continue
@@ -101,41 +128,43 @@ def main():
             lemma = idstringy.split("+")[0]
             lemmas.add(lemma)
     lines = 0
-    forms = 0
     oovs = 0
     for lemma in lemmas:
-        for paradigm in paradigms:
-            generations = generator.lookup(lemma + paradigm)
+        if lemma in {"", "#", "#;"}:
+            continue
+        failed = True
+        for tagstring in options.tags:
+            generations = generator.lookup(lemma + tagstring,
+                                           time_cutoff=0.1)
             if len(generations) == 0:
-                ignoring = False
-                if skiptags:
-                    for skip in skiptags:
-                        if skip in paradigm.split("+"):
-                            ignoring = True
-                            break
-                if skipforms:
-                    if lemma + paradigm in skipforms:
-                        ignoring = True
-                if not ignoring:
-                    if options.verbose:
-                        print(f"{lemma}{paradigm} does not generate!")
-                    print(f"{lemma}{paradigm}", file=logfile)
-                    oovs += 1
-            lines += 1
-            forms += len(generations)
-            if options.debug:
-                print(f"{lemma}{paradigm}:")
-                for g in generations:
-                    print(f"\t{g}")
+                if options.verbose:
+                    print(f"{lemma}{tagstring} does not generate!")
+            else:
+                failed = False
+        if skipforms and lemma in skipforms:
+            continue
+        lines += 1
+        if failed:
+            oovs += 1
+            for tagstring in options.tags:
+                print(f"{lemma}{tagstring}", file=logfile)
+            analyses = analyser.lookup(lemma,
+                                       time_cutoff=0.1)
+            if len(analyses) > 0:
+                print(f"\tN.B: {lemma} has following analyses", file=logfile)
+                for analysis in analyses:
+                    print(f"\t{analysis}", file=logfile)
+            if oovs >= options.oov_limit:
+                print("too many fails, bailing to save time...")
+                break
     if lines == 0:
         print(f"SKIP: could not find lemmas in {options.lexcfile.name}")
         sys.exit(77)
     coverage = (1.0 - (float(oovs) / float(lines))) * 100.0
     if options.verbose:
-        print("Generation statistics:")
-        print(f"\t{len(lemmas)} lemmas × {len(paradigms)} paradigm slots")
-        print(f"\t(should be minimum {len(lemmas)*len(paradigms)} forms then)")
-        print(f"\t{forms} generated, {coverage} % success")
+        print("Lemma statistics:")
+        print(f"\t{len(lemmas)} lemmas")
+        print(f"\t{coverage} % success")
     if coverage < options.threshold:
         print("FAIL: too many lemmas weren't generating!",
               f"{coverage} < {options.threshold}")

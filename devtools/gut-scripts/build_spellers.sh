6278ccda47bbc252d24fb3d4981fe3cf3b4ce9ba
Sjur N Moshagen <sjurnm@mac.com>
2026-04-08 09:39:16 +0300

    Add script to build spellers out of source

---
+++ devtools/gut-scripts/build_spellers.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# gut apply script to run autogen.sh and configure with defaults, to update
+# generated and committed files like manifest.toml:
+
+# command:
+./autogen.sh && mkdir -p bygg/rett/ && ../../configure --enable-spellers && make -j && make check -j

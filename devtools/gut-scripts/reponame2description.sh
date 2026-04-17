d9945e3ffb1905497e933fd8a32ea8ff9c726959
Dylan Hand <dylan.hand@gmail.com>
2026-04-17 12:25:47 +0200

    Update reponame2description to support keyboard- and dict- repos

--- devtools/gut-scripts/reponame2description.sh
+++ devtools/gut-scripts/reponame2description.sh
@@ -9,7 +9,28 @@ orgname=$2
 # Other variables:
 selfdir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
+prefix=$(echo $reponame | cut -d'-' -f1)
 langcode=$(echo $reponame | cut -d'-' -f2)
 langname=$( $selfdir/../../scripts/iso639-to-name.sh $langcode )
 
-printf "Finite state and Constraint Grammar based analysers and proofing tools, and language resources for the $langname language"
+case "$prefix" in
+    keyboard)
+        printf "Keyboard layouts for the $langname language"
+        ;;
+    dict)
+        targetcode=$(echo $reponame | cut -d'-' -f3)
+        targetname=$( $selfdir/../../scripts/iso639-to-name.sh $targetcode )
+        printf "An electronic dictionary from $langname to $targetname"
+        ;;
+    shared)
+        script=$(echo $reponame | cut -d'-' -f3)
+        if [ -n "$script" ]; then
+            printf "Shared $langname ($script) linguistic resources"
+        else
+            printf "Shared $langname linguistic resources"
+        fi
+        ;;
+    lang|*)
+        printf "Finite state and Constraint Grammar based analysers and proofing tools, and language resources for the $langname language"
+        ;;
+esac
